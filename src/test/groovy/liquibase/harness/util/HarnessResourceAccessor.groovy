45015ea1080bab9bf1c3f490052e77a077d9e1c1
Oleh Kushniryk <kushnirykoleh@gmail.com>
2020-08-20 12:48:08 +0300

    fixed path in resourceAccessor

--- src/test/groovy/liquibase/harness/util/HarnessResourceAccessor.groovy
+++ src/test/groovy/liquibase/harness/util/HarnessResourceAccessor.groovy
@@ -7,7 +7,7 @@ class HarnessResourceAccessor extends ClassLoaderResourceAccessor {
     HarnessResourceAccessor() throws Exception {
         super(new URLClassLoader(new URL[]{
                 new File(System.getProperty("user.dir")).toURI().toURL(),
-                new File("src/main/resources/").toURI().toURL(),
+                new File("src/test/resources/").toURI().toURL(),
                 new File(System.getProperty("java.io.tmpdir")).toURI().toURL(),
         }))
     }
