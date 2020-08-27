022a3c9aeb897b2f14371ec230722f58931c6101
Robert Reeves <r2@datical.com>
2020-08-27 10:04:55 -0500

    Added renameTable test for PostgreSQL

--- src/test/resources/expectedSql/postgresql/renameTable.sql
+++ src/test/resources/expectedSql/postgresql/renameTable.sql
@@ -1,1 +1,2 @@
-CREATE TABLE public.oldnametable (test_id INTEGER NOT NULL, test_column VARCHAR(50) NOT NULL, CONSTRAINT OLDNAMETABLE_PKEY PRIMARY KEY (test_id)), ALTER TABLE public.oldnametable RENAME TO newnametable
+CREATE TABLE public.oldnametable (test_id INTEGER NOT NULL, test_column VARCHAR(50) NOT NULL, CONSTRAINT OLDNAMETABLE_PKEY PRIMARY KEY (test_id))
+ALTER TABLE public.oldnametable RENAME TO newnametable
