83c431eb055d9394549006e1cbf7effeeae6a15a
Oleh Kushniryk <kushnirykoleh@gmail.com>
2020-08-27 13:49:54 +0300

    added renameTable and renameColumn for postgresql

--- src/test/resources/changelogs/postgresql/renameTable.sql
+++ src/test/resources/changelogs/postgresql/renameTable.sql
@@ -1,1 +1,7 @@
-ALTER  TABLE  authors  RENAME creators;
+--liquibase formatted sql
+--changeset oleh:1
+-- Database: postgresql
+-- Change Parameter: newTableName=creators
+-- Change Parameter: oldTableName=authors
+ALTER TABLE authors RENAME TO creators;
+--rollback ALTER TABLE creators RENAME TO authors;
