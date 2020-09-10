df8e7bd9733b33bf31cd1e8dfb603ad23486c1f3
kristyldatical <31962545+kristyldatical@users.noreply.github.com>
2020-09-10 10:06:40 -0500

    Adding comment to indicate that this test is

--- src/test/resources/expectedSql/postgresql/datatypes12.sql
+++ src/test/resources/expectedSql/postgresql/datatypes12.sql
@@ -1,1 +1,2 @@
+-- This test was created to reproduce a Liquibase bug. The sqlGenerator is producing "serial INTEGER" instead of "serial SERIAL")
 CREATE TABLE public.datatypes12_test_table (serial SERIAL, bigserial BIGSERIAL)
