3bd62a2af5134cd8ddcfcb4f569cfa25a75aa5b2
Oleh Kushniryk <kushnirykoleh@gmail.com>
2020-10-29 18:30:29 +0200

    fixed default inputFormat in yml file

--- src/test/groovy/liquibase/sdk/test/LiquibaseSdkSuite.groovy
+++ src/test/groovy/liquibase/sdk/test/LiquibaseSdkSuite.groovy
@@ -2,7 +2,7 @@ package liquibase.sdk.test
 
 import liquibase.sdk.test.config.TestConfig
 
-class LiquibaseSdkTest extends BaseLiquibaseSdkSuite {
+class LiquibaseSdkSuite extends BaseLiquibaseSdkSuite {
 
     static {
         //extensions don't distribute their files. Only we store them in src/main/resources.
