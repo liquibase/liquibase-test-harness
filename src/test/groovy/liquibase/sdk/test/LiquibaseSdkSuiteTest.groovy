8a2b197af259c52ac964344cd9c8a1bfea510f50
kristyldatical <kgomes@datical.com>
2020-11-06 19:16:08 -0600

    Adding changes for mvn test execution - failsafe plugin

--- src/test/groovy/liquibase/sdk/test/LiquibaseSdkSuiteTest.groovy
+++ src/test/groovy/liquibase/sdk/test/LiquibaseSdkSuiteTest.groovy
@@ -2,7 +2,7 @@ package liquibase.sdk.test
 
 import liquibase.sdk.test.config.TestConfig
 
-class LiquibaseSdkSuite extends BaseLiquibaseSdkSuite {
+class LiquibaseSdkSuiteTest extends BaseLiquibaseSdkSuite {
 
     static {
         //extensions don't distribute their files. Only we store them in src/main/resources.
