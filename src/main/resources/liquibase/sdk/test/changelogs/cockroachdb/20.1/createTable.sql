7411fa6341340c566011e555ed4ebd6410196c2c
kristyldatical <kgomes@datical.com>
2020-11-09 22:36:49 -0600

    Making updates to fix cockroachdb tests

--- src/main/resources/liquibase/sdk/test/changelogs/cockroachdb/20.1/createTable.sql
+++ src/main/resources/liquibase/sdk/test/changelogs/cockroachdb/20.1/createTable.sql
@@ -1,8 +1,8 @@
 --liquibase formatted sql
 
 --changeset kristyl:1
-create table cockroachdb_table (
-  id_column int not null primary key,
-  name_column varchar(255)
+create table test_table (
+  test_id int not null primary key,
+  test_column varchar(50)
 );
---rollback DROP TABLE cockroachdb_table
+--rollback DROP TABLE test_table
