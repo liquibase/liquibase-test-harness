38279b09140e05a5d8ffbbd70d865d1e051f7965
Rafi Shamim <rafi@cockroachlabs.com>
2020-11-12 01:43:58 -0500

    Fix CockroachDB tests

--- src/main/resources/liquibase/sdk/test/changelogs/cockroachdb/20.1/createView.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
-
---changeset kristyl:3
-create view test_view as select id, first_name, last_name, email from lbcat.authors;
---rollback drop view test_view
---
+++ src/main/resources/liquibase/sdk/test/expectedSql/cockroachdb/dropPrimaryKey.sql
@@ -0,0 +1,7 @@
+INVALID TEST
+
+CockroachDB does not allow dropping a primary key unless a new one
+is created in the same transaction.
+
+See https://www.cockroachlabs.com/docs/stable/drop-constraint.html#drop-and-add-a-primary-key-constraint
+
