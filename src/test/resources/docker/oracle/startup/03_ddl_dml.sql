d38ed25d00c6fd7fe7e797823b75b439044be2b5
Robert Reeves <r2@liquibase.com>
2020-12-17 10:26:59 -0600

    oracle 21c updates

--- src/test/resources/docker/oracle/startup/03_ddl_dml.sql
+++ src/test/resources/docker/oracle/startup/03_ddl_dml.sql
@@ -2,7 +2,7 @@
 --  DDL for Table AUTHORS
 --------------------------------------------------------
 
-  CREATE TABLE "C##LIQUIBASE"."AUTHORS" 
+  CREATE TABLE "LIQUIBASE"."AUTHORS" 
    (	"ID" NUMBER(*,0), 
 	"FIRST_NAME" VARCHAR2(50 BYTE), 
 	"LAST_NAME" VARCHAR2(50 BYTE), 
@@ -17,20 +17,20 @@
 --  DDL for Index AUTHORS_PK
 --------------------------------------------------------
 
-  CREATE UNIQUE INDEX "C##LIQUIBASE"."AUTHORS_PK" ON "C##LIQUIBASE"."AUTHORS" ("ID") 
+  CREATE UNIQUE INDEX "LIQUIBASE"."AUTHORS_PK" ON "LIQUIBASE"."AUTHORS" ("ID") 
   PCTFREE 10 INITRANS 2 MAXTRANS 255 
   TABLESPACE "USERS" ;
 --------------------------------------------------------
 --  Constraints for Table AUTHORS
 --------------------------------------------------------
 
-  ALTER TABLE "C##LIQUIBASE"."AUTHORS" MODIFY ("ID" NOT NULL ENABLE);
-  ALTER TABLE "C##LIQUIBASE"."AUTHORS" MODIFY ("FIRST_NAME" NOT NULL ENABLE);
-  ALTER TABLE "C##LIQUIBASE"."AUTHORS" MODIFY ("LAST_NAME" NOT NULL ENABLE);
-  ALTER TABLE "C##LIQUIBASE"."AUTHORS" MODIFY ("EMAIL" NOT NULL ENABLE);
-  ALTER TABLE "C##LIQUIBASE"."AUTHORS" MODIFY ("BIRTHDATE" NOT NULL ENABLE);
-  ALTER TABLE "C##LIQUIBASE"."AUTHORS" MODIFY ("ADDED" NOT NULL ENABLE);
-  ALTER TABLE "C##LIQUIBASE"."AUTHORS" ADD CONSTRAINT "AUTHORS_PK" PRIMARY KEY ("ID")
+  ALTER TABLE "LIQUIBASE"."AUTHORS" MODIFY ("ID" NOT NULL ENABLE);
+  ALTER TABLE "LIQUIBASE"."AUTHORS" MODIFY ("FIRST_NAME" NOT NULL ENABLE);
+  ALTER TABLE "LIQUIBASE"."AUTHORS" MODIFY ("LAST_NAME" NOT NULL ENABLE);
+  ALTER TABLE "LIQUIBASE"."AUTHORS" MODIFY ("EMAIL" NOT NULL ENABLE);
+  ALTER TABLE "LIQUIBASE"."AUTHORS" MODIFY ("BIRTHDATE" NOT NULL ENABLE);
+  ALTER TABLE "LIQUIBASE"."AUTHORS" MODIFY ("ADDED" NOT NULL ENABLE);
+  ALTER TABLE "LIQUIBASE"."AUTHORS" ADD CONSTRAINT "AUTHORS_PK" PRIMARY KEY ("ID")
   USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 
   TABLESPACE "USERS"  ENABLE;
 
@@ -38,17 +38,17 @@
 --  insert data into authors
 --------------------------------------------------------
 
-INSERT INTO "C##LIQUIBASE"."AUTHORS" VALUES('1','Eileen','Lubowitz','ppaucek@example.org','04-MAR-91', CURRENT_TIMESTAMP);
-INSERT INTO "C##LIQUIBASE"."AUTHORS" VALUES('2','Tamia','Mayert','shansen@example.org','27-MAR-16', CURRENT_TIMESTAMP);
-INSERT INTO "C##LIQUIBASE"."AUTHORS" VALUES('3','Cyril','Funk','reynolds.godfrey@example.com','21-APR-88', CURRENT_TIMESTAMP);
-INSERT INTO "C##LIQUIBASE"."AUTHORS" VALUES('4','Nicolas','Buckridge','xhoeger@example.net','03-MAR-17', CURRENT_TIMESTAMP);
-INSERT INTO "C##LIQUIBASE"."AUTHORS" VALUES('5','Jayden','Walter','lillian66@example.com','27-FEB-10', CURRENT_TIMESTAMP);
+INSERT INTO "LIQUIBASE"."AUTHORS" VALUES('1','Eileen','Lubowitz','ppaucek@example.org','04-MAR-91', CURRENT_TIMESTAMP);
+INSERT INTO "LIQUIBASE"."AUTHORS" VALUES('2','Tamia','Mayert','shansen@example.org','27-MAR-16', CURRENT_TIMESTAMP);
+INSERT INTO "LIQUIBASE"."AUTHORS" VALUES('3','Cyril','Funk','reynolds.godfrey@example.com','21-APR-88', CURRENT_TIMESTAMP);
+INSERT INTO "LIQUIBASE"."AUTHORS" VALUES('4','Nicolas','Buckridge','xhoeger@example.net','03-MAR-17', CURRENT_TIMESTAMP);
+INSERT INTO "LIQUIBASE"."AUTHORS" VALUES('5','Jayden','Walter','lillian66@example.com','27-FEB-10', CURRENT_TIMESTAMP);
 
 --------------------------------------------------------
 --  DDL for Table POSTS
 --------------------------------------------------------
 
-  CREATE TABLE "C##LIQUIBASE"."POSTS" 
+  CREATE TABLE "LIQUIBASE"."POSTS" 
    (	"ID" NUMBER(*,0), 
 	"AUTHOR_ID" NUMBER(*,0), 
 	"TITLE" VARCHAR2(255 BYTE), 
@@ -64,20 +64,20 @@ INSERT INTO "C##LIQUIBASE"."AUTHORS" VALUES('5','Jayden','Walter','lillian66@exa
 --  DDL for Index POSTS_PK
 --------------------------------------------------------
 
-  CREATE UNIQUE INDEX "C##LIQUIBASE"."POSTS_PK" ON "C##LIQUIBASE"."POSTS" ("ID") 
+  CREATE UNIQUE INDEX "LIQUIBASE"."POSTS_PK" ON "LIQUIBASE"."POSTS" ("ID") 
   PCTFREE 10 INITRANS 2 MAXTRANS 255 
   TABLESPACE "USERS" ;
 --------------------------------------------------------
 --  Constraints for Table POSTS
 --------------------------------------------------------
 
-  ALTER TABLE "C##LIQUIBASE"."POSTS" MODIFY ("ID" NOT NULL ENABLE);
-  ALTER TABLE "C##LIQUIBASE"."POSTS" MODIFY ("AUTHOR_ID" NOT NULL ENABLE);
-  ALTER TABLE "C##LIQUIBASE"."POSTS" MODIFY ("TITLE" NOT NULL ENABLE);
-  ALTER TABLE "C##LIQUIBASE"."POSTS" MODIFY ("DESCRIPTION" NOT NULL ENABLE);
-  ALTER TABLE "C##LIQUIBASE"."POSTS" MODIFY ("CONTENT" NOT NULL ENABLE);
-  --ALTER TABLE "C##LIQUIBASE"."POSTS" MODIFY ("INSERTED_DATE" NOT NULL ENABLE);
-  ALTER TABLE "C##LIQUIBASE"."POSTS" ADD CONSTRAINT "POSTS_PK" PRIMARY KEY ("ID")
+  ALTER TABLE "LIQUIBASE"."POSTS" MODIFY ("ID" NOT NULL ENABLE);
+  ALTER TABLE "LIQUIBASE"."POSTS" MODIFY ("AUTHOR_ID" NOT NULL ENABLE);
+  ALTER TABLE "LIQUIBASE"."POSTS" MODIFY ("TITLE" NOT NULL ENABLE);
+  ALTER TABLE "LIQUIBASE"."POSTS" MODIFY ("DESCRIPTION" NOT NULL ENABLE);
+  ALTER TABLE "LIQUIBASE"."POSTS" MODIFY ("CONTENT" NOT NULL ENABLE);
+  --ALTER TABLE "LIQUIBASE"."POSTS" MODIFY ("INSERTED_DATE" NOT NULL ENABLE);
+  ALTER TABLE "LIQUIBASE"."POSTS" ADD CONSTRAINT "POSTS_PK" PRIMARY KEY ("ID")
   USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 
   TABLESPACE "USERS"  ENABLE;
 
@@ -85,9 +85,9 @@ INSERT INTO "C##LIQUIBASE"."AUTHORS" VALUES('5','Jayden','Walter','lillian66@exa
 --  insert data into posts
 --------------------------------------------------------
 
-INSERT INTO "C##LIQUIBASE"."POSTS" VALUES('1','1','temporibus','voluptatum','Fugit non et doloribus repudiandae.', CURRENT_TIMESTAMP);
-INSERT INTO "C##LIQUIBASE"."POSTS" VALUES('2','2','ea','aut','Tempora molestias maiores provident molestiae sint possimus quasi.',CURRENT_TIMESTAMP);
-INSERT INTO "C##LIQUIBASE"."POSTS" VALUES('3','3','illum','rerum','Delectus recusandae sit officiis dolor.', CURRENT_TIMESTAMP);
-INSERT INTO "C##LIQUIBASE"."POSTS" VALUES('4','4','itaque','deleniti','Magni nam optio id recusandae.', CURRENT_TIMESTAMP);
-INSERT INTO "C##LIQUIBASE"."POSTS" VALUES('5','5','ad','similique','Rerum tempore quis ut nesciunt qui excepturi est.', CURRENT_TIMESTAMP);
+INSERT INTO "LIQUIBASE"."POSTS" VALUES('1','1','temporibus','voluptatum','Fugit non et doloribus repudiandae.', CURRENT_TIMESTAMP);
+INSERT INTO "LIQUIBASE"."POSTS" VALUES('2','2','ea','aut','Tempora molestias maiores provident molestiae sint possimus quasi.',CURRENT_TIMESTAMP);
+INSERT INTO "LIQUIBASE"."POSTS" VALUES('3','3','illum','rerum','Delectus recusandae sit officiis dolor.', CURRENT_TIMESTAMP);
+INSERT INTO "LIQUIBASE"."POSTS" VALUES('4','4','itaque','deleniti','Magni nam optio id recusandae.', CURRENT_TIMESTAMP);
+INSERT INTO "LIQUIBASE"."POSTS" VALUES('5','5','ad','similique','Rerum tempore quis ut nesciunt qui excepturi est.', CURRENT_TIMESTAMP);
    
