ce6b2475e296a51100b98f627b118992cdc4444c
Ravi Devarakonda <ravi041282@gmail.com>
2021-02-23 13:16:23 +0000

    add provider parameters for auth with config

--- src/test/resources/terraform/dbs/provider.tf
+++ src/test/resources/terraform/dbs/provider.tf
@@ -1,4 +1,18 @@
-provider oci {
+terraform {
+  required_providers {
+    oci = {
+      source = "hashicorp/oci"
+      #version = "4.14.0"
+    }
+  }
+}
+
+provider "oci" {
+  # Configuration options
   auth = "InstancePrincipal"
   region = var.region
+  #tenancy_ocid = var.tenancy_ocid
+  #user_ocid = var.user_ocid
+  #fingerprint = var.fingerprint
+  #private_key_path = var.private_key_path
 }
