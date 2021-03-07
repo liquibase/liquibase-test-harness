0a94de1c8263cc06ab15f3b1646bb7991d867d80
kristyldatical <kgomes@liquibase.com>
2021-03-07 12:55:23 -0600

    createView failure needs to be further investigated

---
+++ src/main/resources/liquibase/harness/change/expectedSql/edb/createView.sql
@@ -0,0 +1,3 @@
+INVALID TEST
+-- This does not appear to be a genuine failure, the test is failing because the expected snapshot file is not found when it in fact is present
+-- This is needs to be investigated further by Oleh
