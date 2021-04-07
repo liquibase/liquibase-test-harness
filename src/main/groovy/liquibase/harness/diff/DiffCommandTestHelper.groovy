b5dcbfd0286de551ed7744cdfbe6607354899ba7
Oleh Kushniryk <kushnirykoleh@gmail.com>
2021-04-07 16:13:59 +0300

    added foreignKeys, views, uniqueConstraints checks

--- src/main/groovy/liquibase/harness/diff/DiffCommandTestHelper.groovy
+++ src/main/groovy/liquibase/harness/diff/DiffCommandTestHelper.groovy
@@ -8,11 +8,14 @@ import liquibase.diff.output.DiffOutputControl
 import liquibase.diff.output.changelog.DiffToChangeLog
 import liquibase.harness.config.DatabaseUnderTest
 import liquibase.harness.config.TestConfig
+import liquibase.structure.AbstractDatabaseObject
 import liquibase.structure.core.Column
+import liquibase.structure.core.ForeignKey
 import liquibase.structure.core.Index
 import liquibase.structure.core.PrimaryKey
 import liquibase.structure.core.Table
-import liquibase.util.StreamUtil
+import liquibase.structure.core.UniqueConstraint
+import liquibase.structure.core.View
 import org.yaml.snakeyaml.Yaml
 
 class DiffCommandTestHelper {
@@ -58,7 +61,7 @@ class DiffCommandTestHelper {
     }
 
     static boolean removeAndCheckExpectedDiffs(ExpectedDiffs expectedDiffs, DiffResult diffResult){
-        //TODO finish this, think bout other objects -views foreignKeys, UniqueConstraints, storedProcedures, sequences
+        //TODO finish this, think bout other objects , UniqueConstraints
         // refactor to move Predicated outside
 
         Set<Table> unexpectedTables = diffResult.getUnexpectedObjects(Table.class)
@@ -70,8 +73,7 @@ class DiffCommandTestHelper {
         missingTables.removeIf({ table -> expectedDiffs.missingObjects?.contains(table.name) })
 
         Map<Table, ObjectDifferences> changedTables = diffResult.getChangedObjects(Table.class)
-
-        changedTables.entrySet().removeIf({ entry -> expectedDiffs.changedObjects?.contains(entry.key.toString()) })
+        removeChangedObjects(changedTables, expectedDiffs)
 
 
         Set<Column> unexpectedColumns = diffResult.getUnexpectedObjects(Column.class)
@@ -84,8 +86,7 @@ class DiffCommandTestHelper {
 
         Map<Column, ObjectDifferences> changedColumns = diffResult.getChangedObjects(Column.class)
         changedColumns.entrySet().removeIf({ entry -> "DATABASECHANGELOGLOCK".equalsIgnoreCase(entry.key.relation.name) || "DATABASECHANGELOG".equalsIgnoreCase(entry.key.relation.name) })
-        //TODO does toString() suit our case when diff for column is like "lbcat.posts.id", check other cases
-        changedColumns.entrySet().removeIf({ entry -> expectedDiffs.changedObjects?.contains(entry.key.toString()) })
+        removeChangedObjects(changedColumns, expectedDiffs)
 
 
         Set<Index> unexpectedIndexes = diffResult.getUnexpectedObjects(Index.class)
@@ -97,7 +98,7 @@ class DiffCommandTestHelper {
         missingIndexes.removeIf({ index -> expectedDiffs.missingObjects?.contains(index.relation.name) })
 
         Map<Index, ObjectDifferences> changedIndexes = diffResult.getChangedObjects(Index.class)
-        changedIndexes.entrySet().removeIf({ entry -> expectedDiffs.changedObjects.contains(entry.key.relation.name) })
+        removeChangedObjects(changedIndexes, expectedDiffs)
 
 
         Set<PrimaryKey> unexpectedPrimaryKeys = diffResult.getUnexpectedObjects(PrimaryKey.class)
@@ -109,7 +110,37 @@ class DiffCommandTestHelper {
         missingPrimaryKeys.removeIf({ primaryKey -> expectedDiffs.unexpectedObjects?.contains(primaryKey.table.name) })
 
         Map<PrimaryKey, ObjectDifferences> changedPrimaryKeys = diffResult.getChangedObjects(PrimaryKey.class)
-        changedPrimaryKeys.entrySet().removeIf({ entry -> expectedDiffs.changedObjects?.contains(entry.key.table.name) })
+        removeChangedObjects(changedPrimaryKeys, expectedDiffs)
+
+        // tracking tables shouldn't have ForeignKeys, don't need to iterate over. fix otherwise
+        Set<ForeignKey> unexpectedForeignKeys = diffResult.getUnexpectedObjects(ForeignKey.class)
+        unexpectedForeignKeys.removeIf({ foreignKey -> expectedDiffs.unexpectedObjects?.contains(foreignKey.name) })
+
+        Set<ForeignKey> missingForeignKeys = diffResult.getMissingObjects(ForeignKey.class)
+        missingForeignKeys.removeIf({ foreignKey -> expectedDiffs.missingObjects?.contains(foreignKey.name) })
+
+        Map<ForeignKey, ObjectDifferences> changedForeignKeys = diffResult.getChangedObjects(ForeignKey.class)
+        removeChangedObjects(changedForeignKeys, expectedDiffs)
+
+
+        Set<View> unexpectedViews = diffResult.getUnexpectedObjects(View.class)
+        unexpectedViews.removeIf({ view -> expectedDiffs.unexpectedObjects?.contains(view.name) })
+
+        Set<View> missingViews = diffResult.getMissingObjects(View.class)
+        missingViews.removeIf({ view -> expectedDiffs.missingObjects?.contains(view.name) })
+
+        Map<View, ObjectDifferences> changedViews = diffResult.getChangedObjects(View.class)
+        removeChangedObjects(changedViews, expectedDiffs)
+
+
+        Set<UniqueConstraint> unexpectedUniqueConstraints = diffResult.getUnexpectedObjects(UniqueConstraint.class)
+        unexpectedUniqueConstraints.removeIf({ constraint -> expectedDiffs.unexpectedObjects?.contains(constraint.name) })
+
+        Set<UniqueConstraint> missingUniqueConstraints = diffResult.getMissingObjects(UniqueConstraint.class)
+        missingUniqueConstraints.removeIf({ constraint -> expectedDiffs.missingObjects?.contains(constraint.name) })
+
+        Map<UniqueConstraint, ObjectDifferences> changedUniqueConstraints = diffResult.getChangedObjects(UniqueConstraint.class)
+        removeChangedObjects(changedUniqueConstraints, expectedDiffs)
 
         return unexpectedTables.size() == 0 &&
         missingTables.size() == 0 &&
@@ -122,7 +153,20 @@ class DiffCommandTestHelper {
         changedIndexes.size() == 0 &&
         unexpectedPrimaryKeys.size() == 0 &&
         missingPrimaryKeys.size() == 0 &&
-        changedPrimaryKeys.size() == 0
+        changedPrimaryKeys.size() == 0 &&
+        unexpectedForeignKeys.size() == 0 &&
+        missingForeignKeys.size() == 0 &&
+        changedForeignKeys.size() == 0 &&
+        unexpectedViews.size() == 0 &&
+        missingViews.size() == 0 &&
+        changedViews.size() == 0 &&
+        unexpectedUniqueConstraints.size() == 0 &&
+        missingUniqueConstraints.size() == 0 &&
+        changedUniqueConstraints.size() == 0
+    }
+
+    static void removeChangedObjects(Map<AbstractDatabaseObject, ObjectDifferences> map, ExpectedDiffs expectedDiffs){
+         map.entrySet().removeIf({ entry -> expectedDiffs.changedObjects?.contains(entry.key.toString()) })
     }
 
     @Builder
