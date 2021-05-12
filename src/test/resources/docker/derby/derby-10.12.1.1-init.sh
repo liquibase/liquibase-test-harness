21212e0a277b292a54fba95d0263486fb5bdb94d
AlexanderSashchenko <santys87@gmail.com>
2021-05-12 14:19:17 +0300

    Added support for test cases for Apache Derby

---
+++ src/test/resources/docker/derby/derby-10.12.1.1-init.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+. /db-derby-10.12.1.1-bin/bin/ij /docker-entrypoint-initdb.d/input-for-ij.txt;
---
+++ src/test/resources/docker/derby/derby-10.15.1.3-init.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+. /derby/bin/ij /docker-entrypoint-initdb.d/input-for-ij.txt;
