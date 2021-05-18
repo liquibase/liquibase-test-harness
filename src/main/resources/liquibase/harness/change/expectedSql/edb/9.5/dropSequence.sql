3e57b4d9f0afa30234d2c790770d3575b9fd9d3c
Oleh Kushniryk <kushnirykoleh@gmail.com>
2021-05-18 16:42:00 +0300

    fixed tests for EDB 9.5 and 9.6

---
+++ src/main/resources/liquibase/harness/change/expectedSql/edb/9.5/dropSequence.sql
@@ -0,0 +1,6 @@
+-- This test was created to reproduce a Liquibase bug. [AS] syntax is not supported by postgesql in versions prior to 10
+-- https://github.com/liquibase/liquibase/issues/1429
+-- same apply to EDB 9.5 we put INVALID TEST for this not to break a build until issue is fixed
+INVALID TEST
+CREATE SEQUENCE  IF NOT EXISTS test_sequence START WITH 1 INCREMENT BY 1 MINVALUE 1
+DROP SEQUENCE test_sequence CASCADE
