f48453fd6f2107ca34c02639f5cc564af54e4a91
Oleh Kushniryk <kushnirykoleh@gmail.com>
2021-05-19 16:34:12 +0300

    added comments, fixed EDB and cockroachDB tests

--- src/main/resources/liquibase/harness/change/expectedSql/edb/9.5/renameSequence.sql
+++
@@ -1,6 +0,0 @@
--- This test was created to reproduce a Liquibase bug. [AS] syntax is not supported by postgesql in versions prior to 10
--- https://github.com/liquibase/liquibase/issues/1429
--- same apply to EDB 9.5 we put INVALID TEST for this not to break a build until issue is fixed
-INVALID TEST
-CREATE SEQUENCE  IF NOT EXISTS test_sequence START WITH 1 INCREMENT BY 1 MINVALUE 1
-ALTER SEQUENCE test_sequence RENAME TO test_sequence_rename_test
