816c73b075ad5a581f24a27eb37528d6be75d11a
AlexanderSashchenko <santys87@gmail.com>
2021-05-24 13:24:21 +0300

    added supprot for 'delete' change type for all databases supported by test-harness

---
+++ src/main/resources/liquibase/harness/change/expectedSql/oracle/delete.sql
@@ -0,0 +1,2 @@
+INSERT INTO posts (id, author_id, title, description, content) values (6, 6, 'test title', 'test description', 'test content')
+DELETE FROM posts WHERE id=6
---
+++ src/main/resources/liquibase/harness/change/expectedSql/sqlite/delete.sql
@@ -0,0 +1,2 @@
+INSERT INTO posts (id, author_id, title, description, content, inserted_date) values (6, 6, 'test title', 'test description', 'test content', '2006-10-09')
+DELETE FROM posts WHERE id=6
