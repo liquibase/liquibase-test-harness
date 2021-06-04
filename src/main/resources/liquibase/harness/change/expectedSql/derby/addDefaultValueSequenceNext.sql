b6eab9aea39c022978a9d417e2dd393a2174fd97
AlexanderSashchenko <santys87@gmail.com>
2021-06-05 00:20:41 +0300

    Added support for operation with sequences and adding default values against Apache Derby

---
+++ src/main/resources/liquibase/harness/change/expectedSql/derby/addDefaultValueSequenceNext.sql
@@ -0,0 +1,5 @@
+INVALID TEST
+
+The 'NEXT VALUE FOR' expression which is generated during execution of this change set
+is not allowed for ALTER statements for Apache Derby:
+https://db.apache.org/derby/docs/10.15/ref/rrefsqljnextvaluefor.html
---
+++ src/main/resources/liquibase/harness/change/expectedSql/derby/renameSequence.sql
@@ -0,0 +1,2 @@
+INVALID TEST
+Apache Derby doesn't support renaming sequences https://db.apache.org/derby/docs/10.15/ref/
