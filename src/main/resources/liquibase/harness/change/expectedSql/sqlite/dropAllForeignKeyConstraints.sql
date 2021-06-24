d73321fd5f2708a8b737f75ece8a46bbb0d5aa7c
AlexanderSashchenko <santys87@gmail.com>
2021-06-24 15:36:30 +0300

    Created separate changeSet for dropAllForeignKeyConstraints changeLog

--- src/main/resources/liquibase/harness/change/expectedSql/sqlite/dropAllForeignKeyConstraints.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/sqlite/dropAllForeignKeyConstraints.sql
@@ -1,2 +1,2 @@
-ALTER TABLE posts ADD CONSTRAINT fk_posts_authors_test_1 FOREIGN KEY (author_id) REFERENCES authors (id)
-ALTER TABLE posts ADD CONSTRAINT fk_posts_authors_test_2 FOREIGN KEY (id) REFERENCES authors (id)
+INVALID TEST
+--Liquibase doesn't support this changeType for SQLite
