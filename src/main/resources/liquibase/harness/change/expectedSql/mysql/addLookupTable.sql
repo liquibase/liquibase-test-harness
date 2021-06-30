5c62b7f0310a000b80b0817cdaac0ec152efe428
AlexanderSashchenko <santys87@gmail.com>
2021-06-30 17:54:18 +0300

    Fixed datatypes issue for mysql and mariadb

--- src/main/resources/liquibase/harness/change/expectedSql/mysql/addLookupTable.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/mysql/addLookupTable.sql
@@ -1,4 +1,4 @@
-CREATE TABLE posts_details AS SELECT DISTINCT inserted_date AS post_date FROM posts WHERE inserted_date IS NOT NULL
-ALTER TABLE posts_details MODIFY post_date date NOT NULL
-ALTER TABLE posts_details ADD PRIMARY KEY (post_date)
-ALTER TABLE posts ADD CONSTRAINT FK_POSTS_POSTS_DETAILS FOREIGN KEY (inserted_date) REFERENCES posts_details (post_date)
+CREATE TABLE authors_data AS SELECT DISTINCT email AS authors_email FROM authors WHERE email IS NOT NULL
+ALTER TABLE authors_data MODIFY authors_email VARCHAR(100) COLLATE utf8_unicode_ci NOT NULL
+ALTER TABLE authors_data ADD PRIMARY KEY (authors_email)
+ALTER TABLE authors ADD CONSTRAINT FK_AUTHORS_AUTHORS_DATA FOREIGN KEY (email) REFERENCES authors_data (authors_email)
