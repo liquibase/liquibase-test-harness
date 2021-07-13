f8bf74542671e164bd6d7481102cb638855ebb6d
Oleh Kushniryk <kushnirykoleh@gmail.com>
2021-07-13 15:21:13 +0300

    fixed sequence tests for postgre 9 & 9.5

--- src/main/resources/liquibase/harness/change/expectedSql/postgresql/9.5/createSequence.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/postgresql/9.5/createSequence.sql
@@ -1,3 +1,3 @@
 -- This test was created to reproduce a Liquibase bug. [AS] syntax is not supported by postgesql in versions prior to 10
 -- https://github.com/liquibase/liquibase/issues/1429
-CREATE SEQUENCE  IF NOT EXISTS test_sequence AS int START WITH 1 INCREMENT BY 1 MINVALUE 1
+CREATE SEQUENCE  IF NOT EXISTS test_sequence START WITH 1 INCREMENT BY 1 MINVALUE 1
