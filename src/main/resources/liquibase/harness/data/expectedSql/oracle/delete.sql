3f817f4ec2f634e7a7bd22817979bdc469baf9ec
AlexanderSashchenko <santys87@gmail.com>
2021-08-27 15:46:52 +0300

    Added oracle expected sqls and expected result sets

--- src/main/resources/liquibase/harness/data/expectedSql/oracle/delete.sql
+++ src/main/resources/liquibase/harness/data/expectedSql/oracle/delete.sql
@@ -1,2 +1,1 @@
-INSERT INTO C##LIQUIBASE.posts (id, author_id, title, description, content, inserted_date) VALUES ('100', '10', 'value', 'value', 'value', '10-AUG-88')
-DELETE FROM C##LIQUIBASE.posts WHERE id=100
+INSERT INTO "C##LIQUIBASE".posts (id, author_id, title, description, content, inserted_date) VALUES ('100', '10', 'value', 'value', 'value', '10-AUG-88')
