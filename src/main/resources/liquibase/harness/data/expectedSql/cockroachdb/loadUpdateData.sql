8278681a9a921a94116b9e7583fa1f6d9768f27b
AlexanderSashchenko <santys87@gmail.com>
2021-08-27 14:31:04 +0300

    Fixed expected sqls and result sets for change data tests

---
+++ src/main/resources/liquibase/harness/data/expectedSql/cockroachdb/loadUpdateData.sql
@@ -0,0 +1,4 @@
+INVALID TEST
+
+Cockroach has a bug for this changeset (records are inserted in cases where they should be updated)
+https://github.com/liquibase/liquibase/issues/2023
--- src/main/resources/liquibase/harness/data/expectedSql/mssql/loadUpdateData.sql
+++ src/main/resources/liquibase/harness/data/expectedSql/mssql/loadUpdateData.sql
@@ -2,29 +2,29 @@ DECLARE @reccount integer
 SELECT @reccount = count(*) FROM authors WHERE id = 1
 IF @reccount = 0
 BEGIN
-INSERT INTO authors (id, first_name, last_name, email, birthdate, added) VALUES (1, 'Adam', 'Gods', 'test1@example.com', '1000-02-27', '2000-02-04T02:32:00');
+INSERT INTO authors (id, first_name, last_name, email, birthdate, added) VALUES (1, 'Adam', 'Gods', 'test1@example.com', '1000-02-27', '2000-02-04T02:32:00')
 END
 ELSE
 BEGIN
-UPDATE authors SET added = '2000-02-04T02:32:00', birthdate = '1000-02-27', email = 'test1@example.com', first_name = 'Adam', last_name = 'Gods' WHERE id = 1;
+UPDATE authors SET added = '2000-02-04T02:32:00', birthdate = '1000-02-27', email = 'test1@example.com', first_name = 'Adam', last_name = 'Gods' WHERE id = 1
 END
 DECLARE @reccount integer
 SELECT @reccount = count(*) FROM authors WHERE id = 7
 IF @reccount = 0
 BEGIN
-INSERT INTO authors (id, first_name, last_name, email, birthdate, added) VALUES (7, 'Noah', 'Lamekhs', 'test2@example.com', '2000-02-27', '1994-12-10T01:00:00');
+INSERT INTO authors (id, first_name, last_name, email, birthdate, added) VALUES (7, 'Noah', 'Lamekhs', 'test2@example.com', '2000-02-27', '1994-12-10T01:00:00')
 END
 ELSE
 BEGIN
-UPDATE authors SET added = '1994-12-10T01:00:00', birthdate = '2000-02-27', email = 'test2@example.com', first_name = 'Noah', last_name = 'Lamekhs' WHERE id = 7;
+UPDATE authors SET added = '1994-12-10T01:00:00', birthdate = '2000-02-27', email = 'test2@example.com', first_name = 'Noah', last_name = 'Lamekhs' WHERE id = 7
 END
 DECLARE @reccount integer
 SELECT @reccount = count(*) FROM authors WHERE id = 8
 IF @reccount = 0
 BEGIN
-INSERT INTO authors (id, first_name, last_name, email, birthdate, added) VALUES (8, 'Muhammad', 'Ibn Abdullah', 'test3@example.com', '3000-02-27', '2000-12-10T01:00:00');
+INSERT INTO authors (id, first_name, last_name, email, birthdate, added) VALUES (8, 'Muhammad', 'Ibn Abdullah', 'test3@example.com', '3000-02-27', '2000-12-10T01:00:00')
 END
 ELSE
 BEGIN
-UPDATE authors SET added = '2000-12-10T01:00:00', birthdate = '3000-02-27', email = 'test3@example.com', first_name = 'Muhammad', last_name = 'Ibn Abdullah' WHERE id = 8;
+UPDATE authors SET added = '2000-12-10T01:00:00', birthdate = '3000-02-27', email = 'test3@example.com', first_name = 'Muhammad', last_name = 'Ibn Abdullah' WHERE id = 8
 END
