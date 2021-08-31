3a3e3d17559215dba1610f5fa40d9f7e7673c403
Jake Newton <jake@liquibase.com>
2021-08-31 14:34:33 -0500

    DAT-7455 add terraform support to test-harness

--- src/test/resources/terraform/backend.tf
+++ src/test/resources/terraform/backend.tf
@@ -1,3 +1,4 @@
+# The below workspace is used by Liquibase.  You will need to update this to work with your organization.
 terraform {
   backend "remote" {
     organization = "liquibase"
--- src/test/resources/terraform/main.tf
+++ src/test/resources/terraform/main.tf
@@ -1,21 +1,31 @@
+data "http" "ip_address" {
+  url = "https://api.ipify.org"
+  request_headers = {
+    Accept = "text/plain"
+  }
+}
+output "my_public_ip" {
+  value = data.http.ip_address.body
+}
+
+module "db_postgres_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/postgresql"
+
+  name        = "postgres db"
+  description = "Security group for postgres database with port 5432 open to the runner of this plan"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["${data.http.ip_address.body}/32"]
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "test-harness"
   cidr = "10.0.0.0/16"
 
-  azs              = ["us-east-1a", "us-east-1b"]
-  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
-  database_subnets = ["10.0.201.0/24", "10.0.202.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-
-  create_database_subnet_group           = true
-  create_database_subnet_route_table     = true
-  create_database_internet_gateway_route = true
-
+  azs                  = ["us-east-1a", "us-east-1b"]
+  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24"]
   enable_dns_hostnames = true
   enable_dns_support   = true
 
@@ -43,5 +53,10 @@ module "db" {
   username             = "lbuser"
   password             = "LbRootPass1"
   #storage_encrypted   = true
-  subnet_ids = module.vpc.database_subnets
+  subnet_ids             = module.vpc.public_subnets
+  vpc_security_group_ids = [module.db_postgres_sg.security_group_id]
+}
+
+output "dbHost" {
+  value = module.db.db_instance_endpoint
 }
