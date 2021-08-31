fa838374cf2f28cd5d30ba383dbb35e9b8e59e04
Derek Smart <derek@grindaga.com>
2021-08-31 12:24:46 -0400

    bash fixes
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/test/resources/docker/create-infra.sh
+++ src/test/resources/docker/create-infra.sh
@@ -5,14 +5,14 @@ set -ex
 db=$1
 
 # edb setup requires login to private registry
-if [ $db = "edb-9.5" || $db = "edb-9.6" || $db = "edb-10" || $db = "edb-11" || $db = "edb-12" || $db = "edb-13" ]; then
+if [ $db = "edb-9.5" ] || [ $db = "edb-9.6" ] || [ $db = "edb-10" ] || [ $db = "edb-11" ] || [ $db = "edb-12" ] || [ $db = "edb-13" ]; then
   docker login $RT_URL -u $RT_USER -p $RT_PWD
-  docker-compose -f docker-compose.edb.yml up -d edb-13
+  docker-compose -f docker-compose.edb.yml up -d $db
   exit 0
 fi
 
 # crdb also has an init container
-if [ $db = "crdb-20.2" || $db =  "crdb-20.1" || $db = "crdb-21.1" ]; then
+if [ $db = "crdb-20.2" ] || [ $db =  "crdb-20.1" ] || [ $db = "crdb-21.1" ]; then
 docker-compose up -d $db ${db}-init
   exit 0
 fi
