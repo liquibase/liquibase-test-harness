97e00b2cffb1e44aa22beda45e05da3572de58d9
Derek Smart <derek@grindaga.com>
2021-08-31 11:45:34 -0400

    fix bad bash env string
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/test/resources/docker/create-infra.sh
+++ src/test/resources/docker/create-infra.sh
@@ -6,7 +6,7 @@ db=$1
 
 # edb setup requires login to private registry
 if [ $db = "edb-13" ]; then
-  docker login "${{ RT_URL }}" -u "${{ RT_USER }}" -p "${{ RT_PWD }}"
+  docker login $RT_URL -u $RT_USER -p $RT_PWD
   docker-compose -f docker-compose.edb.yml up -d edb-13
   exit 0
 fi
