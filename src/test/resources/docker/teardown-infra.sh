e22555b48fc34feab6e6b31b485089f3d29038cc
Derek Smart <derek@grindaga.com>
2021-08-31 12:24:46 -0400

    bash fixes
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/test/resources/docker/teardown-infra.sh
+++ src/test/resources/docker/teardown-infra.sh
@@ -5,7 +5,7 @@ set -ex
 db=$1
 
 # edb specific teardown
-if [ $db = "edb-9.5" || $db = "edb-9.6" || $db = "edb-10" || $db = "edb-11" || $db = "edb-12" || $db = "edb-13" ]; then
+if [ $db = "edb-9.5" ] || [ $db = "edb-9.6" ] || [ $db = "edb-10" ] || [ $db = "edb-11" ] || [ $db = "edb-12" ] || [ $db = "edb-13" ]; then
    docker-compose -f docker-compose.edb.yml down --volumes
   exit 0
 fi
