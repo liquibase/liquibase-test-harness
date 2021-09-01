bc758becc2493ca754a709788a987c63a7879ba9
Oleh Kushniryk <okush@softserveinc.com>
2021-09-01 16:39:54 +0300

    added files for ChangeObject test for Firebird platform

---
+++ src/main/resources/liquibase/harness/change/expectedSql/firebird/addLookupTable.sql
@@ -0,0 +1,2 @@
+INVALID TEST
+-- firebird doesn't support CREATE AS SELECT - http://www.firebirdfaq.org/faq217/
---
+++ src/main/resources/liquibase/harness/change/expectedSql/firebird/renameSequence.sql
@@ -0,0 +1,3 @@
+INVALID TEST
+-- Liquibase doesn't support sequence rename for Firebird
+-- https://docs.liquibase.com/change-types/community/rename-sequence.html
