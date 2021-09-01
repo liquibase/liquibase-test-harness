f3d2e0efce51bb8f4eaa13ba90bf53b442209222
Derek Smart <derek@grindaga.com>
2021-09-01 11:21:01 -0400

    more bash cases
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/test/resources/docker/teardown-infra.sh
+++ src/test/resources/docker/teardown-infra.sh
@@ -4,16 +4,20 @@ set -ex
 
 db=$1
 
-# edb specific teardown
-if [ $db = "edb-9.5" ] || [ $db = "edb-9.6" ] || [ $db = "edb-10" ] || [ $db = "edb-11" ] || [ $db = "edb-12" ] || [ $db = "edb-13" ]; then
-   docker-compose -f docker-compose.edb.yml down --volumes
-  exit 0
-fi
+case $db in
+  # edb sspecific teardown
+  "edb-9.5"|"edb-9.6"|"edb-10"|"edb-11"|"edb-12"|"edb-13")
+    docker-compose -f docker-compose.edb.yml down --volumes
+    exit 0
+    ;;
 
-# in memory databases
-if [ $db = "derby" ] || [ $db = "sqlite" ]; then
-  exit 0
-fi
+  # in memory databases
+  "derby"|"sqlite")
+    exit 0
+    ;;
 
-# standard teardown
-docker-compose down --volumes
+  # standard teardown
+  *)
+    docker-compose down --volumes
+    ;;
+esac
