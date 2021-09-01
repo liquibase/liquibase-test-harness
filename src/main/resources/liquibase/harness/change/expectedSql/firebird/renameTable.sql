5560ab99d5eb5bd907d9f877ddcd210e80cc2937
Oleh Kushniryk <okush@softserveinc.com>
2021-09-01 16:39:54 +0300

    added files for ChangeObject test for Firebird platform

---
+++ src/main/resources/liquibase/harness/change/expectedSql/firebird/renameTable.sql
@@ -0,0 +1,4 @@
+INVALID TEST
+-- Firebird doesn't support table rename
+-- https://docs.liquibase.com/change-types/community/rename-table.html
+-- http://www.firebirdfaq.org/faq363/
---
+++ src/main/resources/liquibase/harness/change/expectedSql/firebird/setTableRemarks.sql
@@ -0,0 +1,3 @@
+INVALID TEST
+-- Liquibase doesn't support this change type for Firebird
+-- https://docs.liquibase.com/change-types/community/set-table-remarks.html
