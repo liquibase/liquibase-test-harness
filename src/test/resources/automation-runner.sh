824fb6739afea5af0938a3278df7b4bcd2b7bf0e
Derek Smart <derek@grindaga.com>
2021-09-01 11:24:34 -0400

    maven batch mode
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/test/resources/automation-runner.sh
+++ src/test/resources/automation-runner.sh
@@ -11,79 +11,79 @@ test_command () {
 
 case $db in
   "mysql-5.6" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=mysql -DdbVersion=5.6 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=mysql -DdbVersion=5.6 test
     ;;
   "mysql-5.7" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=mysql -DdbVersion=5.7 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=mysql -DdbVersion=5.7 test
     ;;
   "mysql-8" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=mysql -DdbVersion=8 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=mysql -DdbVersion=8 test
     ;;
   "postgres-9.5" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=9.5 -Dmaven.test.failure.ignore=true test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=9.5 -Dmaven.test.failure.ignore=true test
     ;;
   "postgres-9" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=9 -Dmaven.test.failure.ignore=true test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=9 -Dmaven.test.failure.ignore=true test
     ;;
   "postgres-10" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=10 -Dmaven.test.failure.ignore=true test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=10 -Dmaven.test.failure.ignore=true test
     ;;
   "postgres-11" )
-    test_command  mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=11 -Dmaven.test.failure.ignore=true test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=11 -Dmaven.test.failure.ignore=true test
     ;;
   "postgres-12" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=12 -Dmaven.test.failure.ignore=true test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=12 -Dmaven.test.failure.ignore=true test
     ;;
   "postgres-13" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=13 -Dmaven.test.failure.ignore=true test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=postgresql -DdbVersion=13 -Dmaven.test.failure.ignore=true test
     ;;
   "mariadb-10.3" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=mariadb -DdbVersion=10.3 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=mariadb -DdbVersion=10.3 test
     ;;
   "mariadb-10.5" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=mariadb -DdbVersion=10.5 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=mariadb -DdbVersion=10.5 test
     ;;
   "mssql-2017" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=mssql -DdbVersion=2017 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=mssql -DdbVersion=2017 test
     ;;
   "mssql-2019" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=mssql -DdbVersion=2019 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=mssql -DdbVersion=2019 test
     ;;
   "H2Database-1.4" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=h2 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=h2 test
     ;;
   "crdb-20.2" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=cockroachdb -DdbVersion=20.2 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=cockroachdb -DdbVersion=20.2 test
     ;;
   "crdb-20.1" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=cockroachdb -DdbVersion=20.1 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=cockroachdb -DdbVersion=20.1 test
     ;;
   "crdb-21.1" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=cockroachdb -DdbVersion=21.1 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=cockroachdb -DdbVersion=21.1 test
     ;;
   "edb-9.5" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=9.5 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=9.5 test
     ;;
   "edb-9.6" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=9.6 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=9.6 test
     ;;
   "edb-10" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=10 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=10 test
     ;;
   "edb-11" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=11 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=11 test
     ;;
   "edb-12" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=12 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=12 test
     ;;
   "edb-13" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=13 test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=13 test
     ;;
   "derby" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=derby test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=derby test
     ;;
   "sqlite" )
-    test_command mvn -Dtest=LiquibaseHarnessSuiteTest -DdbName=sqlite -Dmaven.test.failure.ignore=true test
+    test_command mvn -B -Dtest=LiquibaseHarnessSuiteTest -DdbName=sqlite -Dmaven.test.failure.ignore=true test
     ;;
 esac
 
