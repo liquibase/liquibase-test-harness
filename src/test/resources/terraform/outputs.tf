8eb705f17b93352d7428df3abec3de1b2d31e1d1
Jake Newton <jake@liquibase.com>
2021-09-10 09:55:37 -0500

    DAT-7455 reset

--- src/test/resources/terraform/outputs.tf
+++ src/test/resources/terraform/outputs.tf
@@ -1,21 +1,21 @@
 # Outputs to use in the harness-config.yml
-output "dbEndpoint" {
-  value = module.db.db_instance_endpoint
-}
-output "dbUsername" {
-  value     = module.db.db_instance_username
-  sensitive = true
-}
-output "dbPassword" {
-  value     = module.db.db_instance_password
-  sensitive = true
-}
-output "dbPort" {
-  value = module.db.db_instance_port
-}
-output "dbName" {
-  value = module.db.db_instance_name
-}
-output "dbAddress" {
-  value = module.db.db_instance_address
-}
+# output "dbEndpoint" {
+#   value = module.postgres.*.db_instance_endpoint
+# }
+# output "dbUsername" {
+#   value     = module.postgres.*.db_instance_username
+#   sensitive = true
+# }
+# output "dbPassword" {
+#   value     = module.postgres.*.db_instance_password
+#   sensitive = true
+# }
+# output "dbPort" {
+#   value = module.postgres.*.db_instance_port
+# }
+# output "dbName" {
+#   value = module.postgres.*.db_instance_name
+# }
+# output "dbAddress" {
+#   value = module.postgres.*.db_instance_address
+# }
