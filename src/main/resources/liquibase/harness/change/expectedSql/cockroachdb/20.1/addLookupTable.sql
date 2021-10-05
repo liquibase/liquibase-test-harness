0908ce26283e1acdd3a3f1d4e9d6f47ff3b7a061
KushnirykOleh <kushnirykoleh@gmail.com>
2021-10-05 16:25:06 +0300

    commented firebird in CICI for now

--- src/main/resources/liquibase/harness/change/expectedSql/cockroachdb/20.1/addLookupTable.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/cockroachdb/20.1/addLookupTable.sql
@@ -1,4 +1,4 @@
-INVALID TEST
+IINVALID TEST
 -- Cockroach DB v 20.1 requires an index to be created manually on both columns prior to a foreign_key is created for those columns.
 -- Current changeset doesn't allow to create those indexes manually. Also Cockroach DB is not officially supported by Liquibase.
 -- That's why it is marked as invalid test.
