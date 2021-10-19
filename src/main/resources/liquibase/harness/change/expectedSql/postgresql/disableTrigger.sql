559b375d0b06e83e5dbe50f5d83567ccdb68ae30
AlexanderSashchenko <santys87@gmail.com>
2021-10-19 15:13:01 +0300

    added trigger changetypes tests for mariadb, edb

---
+++ src/main/resources/liquibase/harness/change/expectedSql/postgresql/disableTrigger.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION test_function()
+RETURNS trigger
+AS $$
+BEGIN
+RAISE NOTICE 'Test trigger function created'
+RETURN NEW
+END
+$$
+LANGUAGE plpgsql
+CREATE TRIGGER test_trigger
+BEFORE INSERT ON public.posts
+FOR EACH ROW EXECUTE PROCEDURE test_function()
+ALTER TABLE public.posts  DISABLE TRIGGER test_trigger
