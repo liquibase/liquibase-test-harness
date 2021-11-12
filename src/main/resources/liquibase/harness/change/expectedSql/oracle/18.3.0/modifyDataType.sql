a0e18216896619f769e685f4a7199f4a3ae0766e
AlexanderSashchenko <santys87@gmail.com>
2021-11-12 16:03:30 +0200

    modifyDataType tests added

---
+++ src/main/resources/liquibase/harness/change/expectedSql/oracle/18.3.0/modifyDataType.sql
@@ -0,0 +1,2 @@
+CREATE TABLE DATICAL_ADMIN.modify_data_type_test (intColumn INTEGER, dateColumn date)
+ALTER TABLE DATICAL_ADMIN.modify_data_type_test MODIFY intColumn VARCHAR2(50)
---
+++ src/main/resources/liquibase/harness/change/expectedSql/oracle/addAutoIncrement.sql
@@ -0,0 +1,4 @@
+INVALID TEST
+
+addAutoIncrement is not supported for Oracle
+--https://docs.liquibase.com/change-types/community/add-auto-increment.html
