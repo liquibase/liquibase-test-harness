c9ac661be40e4d9541fa04502e2cbbcb8bb2dd14
Robert Reeves <r2@liquibase.com>
2021-12-15 09:47:19 -0600

    Fixed Percona tests

--- src/main/resources/liquibase/harness/change/expectedSql/percona-xtradb-cluster/createFunction.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/percona-xtradb-cluster/createFunction.sql
@@ -1,6 +1,10 @@
-DROP FUNCTION IF EXISTS test_function
-CREATE FUNCTION test_function()
-RETURNS VARCHAR(20)
-BEGIN
-RETURN 'Hello'
-END
+INVALID TEST
+
+Percona XtraDB Cluster handles functions very differently than MySQL
+
+--DROP FUNCTION IF EXISTS test_function
+--CREATE FUNCTION test_function()
+--RETURNS VARCHAR(20)
+--BEGIN
+--RETURN 'Hello'
+--END
