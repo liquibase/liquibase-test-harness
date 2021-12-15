f6f0d191bfeeb0c3e9924ca50f4a092dcdf64798
Robert Reeves <r2@liquibase.com>
2021-12-15 08:41:14 -0600

    Resolving requests for change

--- src/test/resources/docker/create-infra.sh
+++ src/test/resources/docker/create-infra.sh
@@ -7,7 +7,6 @@ db=$1
 case $db in
 
   # percona xtradb cluster needs a bit more time to start
-  #"edb-9.5"|"edb-9.6"|"edb-10"|"edb-11"|"edb-12"|"edb-13")
   "percona-xtradb-cluster-5.7"|"percona-xtradb-cluster-8.0" )
     docker-compose up -d $db
     sleep 180
