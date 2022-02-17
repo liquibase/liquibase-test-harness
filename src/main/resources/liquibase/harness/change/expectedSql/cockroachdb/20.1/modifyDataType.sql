205e613f0c980eee3ef59e69a7a84836527a86ed
AlexanderSashchenko <santys87@gmail.com>
2022-02-17 17:16:19 +0200

    expected sql fix

--- src/main/resources/liquibase/harness/change/expectedSql/cockroachdb/20.1/modifyDataType.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/cockroachdb/20.1/modifyDataType.sql
@@ -1,2 +1,2 @@
-CREATE TABLE public.modify_data_type_test ("intColumn" INTEGER, "dateColumn" date)
-ALTER TABLE public.modify_data_type_test ALTER COLUMN "intColumn" TYPE VARCHAR(50) USING ("intColumn"::VARCHAR(50))
+INVALID TEST
+This version (20.1) of cockroach db does not support modifying of data types
