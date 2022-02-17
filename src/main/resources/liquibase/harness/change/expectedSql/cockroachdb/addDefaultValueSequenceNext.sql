40c8fd032a3b8f92f350103c3060f75837722c04
AlexanderSashchenko <santys87@gmail.com>
2022-02-17 15:04:38 +0200

    sequences fixes

--- src/main/resources/liquibase/harness/change/expectedSql/cockroachdb/addDefaultValueSequenceNext.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/cockroachdb/addDefaultValueSequenceNext.sql
@@ -1,4 +1,4 @@
-CREATE SEQUENCE  IF NOT EXISTS public.test_sequence START WITH 1 INCREMENT BY 1 MINVALUE 1
+CREATE SEQUENCE  IF NOT EXISTS public.test_sequence INCREMENT BY 1 MINVALUE 1
 ALTER TABLE public.authors ADD sequence_referenced_column INTEGER
 ALTER TABLE public.authors ALTER COLUMN  sequence_referenced_column SET DEFAULT nextval('public.test_sequence')
 ALTER SEQUENCE public.test_sequence OWNED BY public.authors.sequence_referenced_column
