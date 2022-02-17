8a56219ffda0b25819871bd0fce9e2151c00396d
AlexanderSashchenko <santys87@gmail.com>
2022-02-17 17:10:59 +0200

    expected sql fix

--- src/main/resources/liquibase/harness/change/expectedSql/cockroachdb/20.1/modifyDataType.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/cockroachdb/20.1/modifyDataType.sql
@@ -1,3 +1,2 @@
-public.modify_data_type_test ALTER COLUMN "intColumn" TYPE VARCHAR(50) USING ("intColumn"::VARCHAR(50))
 CREATE TABLE public.modify_data_type_test ("intColumn" INTEGER, "dateColumn" date)
 ALTER TABLE public.modify_data_type_test ALTER COLUMN "intColumn" TYPE VARCHAR(50) USING ("intColumn"::VARCHAR(50))
