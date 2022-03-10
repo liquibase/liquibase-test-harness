4277e2e9f599b24b8045c67449f6980ccb377c0b
AlexanderSashchenko <santys87@gmail.com>
2022-03-10 17:56:32 +0200

    remove from suit

--- src/main/groovy/liquibase/harness/BaseHarnessSuite.groovy
+++ src/main/groovy/liquibase/harness/BaseHarnessSuite.groovy
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses([BaseLevelTest, ChangeObjectTests, ChangeDataTests, SnapshotObjectTests])
+@Suite.SuiteClasses([ChangeObjectTests, ChangeDataTests, SnapshotObjectTests])
 abstract class BaseHarnessSuite {
 }
