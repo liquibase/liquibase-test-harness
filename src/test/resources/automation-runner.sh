92d9f8a7e9722c98cae6ce4c6a986ef5ff2bc0ac
AlexanderSashchenko <santys87@gmail.com>
2022-03-10 15:36:24 +0200

    resolve conflicts

--- src/test/resources/automation-runner.sh
+++ src/test/resources/automation-runner.sh
@@ -86,7 +86,7 @@ case $db in
     test_command mvn -ntp -Dtest=LiquibaseHarnessSuiteTest -DdbName=cockroachdb -DdbVersion=21.2 test
     ;;
   "crdb-21.2" )
-    test_command mvn -ntp -Dtest=LiquibaseHarnessSuiteTest -DdbName=cockroachdb -DdbVersion=21.2 -Dmaven.test.failure.ignore=true test
+    test_command mvn -ntp -Dtest=LiquibaseHarnessSuiteTest -DdbName=cockroachdb -DdbVersion=21.2 test
     ;;
   "edb-9.5" )
     test_command mvn -ntp -Dtest=LiquibaseHarnessSuiteTest -DdbName=edb -DdbVersion=9.5 test
