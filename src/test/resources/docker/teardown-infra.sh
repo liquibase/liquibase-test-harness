99c1c640e3405e1ec3c7904c99d8a23eb427a1f6
yodzhubeiskyi <oesevg@gmail.com>
2022-04-15 17:56:05 +0300

    [DAT-9954]Fixed create-infra.sh and teardown-infra.sh

--- src/test/resources/docker/teardown-infra.sh
+++ src/test/resources/docker/teardown-infra.sh
@@ -5,8 +5,9 @@ set -ex
 db=$1
 
 case $db in
-  # edb sspecific teardown
-  "edb-9.5"|"edb-9.6"|"edb-10"|"edb-11"|"edb-12"|"edb-13"|"edb-14")
+  # edb specific teardown
+  "edb-postgres-9.5"|"edb-postgres-9.6"|"edb-postgres-10"|"edb-postgres-11"|"edb-postgres-12"|"edb-postgres-13"|"edb-postgres-14"|
+  "edb-edb-9.5"|"edb-edb-9.6"|"edb-edb-10"|"edb-edb-11"|"edb-edb-12"|"edb-edb-13"|"edb-edb-14")
     docker-compose -f docker-compose.edb.yml down --volumes
     exit 0
     ;;
