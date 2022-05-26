0b2f705328e20f873344937444ce3e710306a09d
KushnirykOleh <kushnirykOleh@gmail.com>
2022-05-26 23:28:39 +0300

    merge Develop into main (#231)
    * Make sure liquibaseBranch is set correctly
    
    * Use new liquibase-sdk-maven-plugin 0.7
    
    * Better standardization of data checks on h2
    
    * Build on pushes to develop
    
    * Better standardization of data checks on h2
    
    * Test that mysql correctly preserves both remarks and primary key info set on created tables
    
    * Test that mysql correctly preserves both remarks and primary key info set on created tables
    
    * Updated setPrimaryKeyColumnRemarks.sql to match updated SQL
    
    * Include test results artifacts for failed tests as well
    
    * [DAT-9954] Created tests for EDB with EDB driver (#209)
    
    * [DAT-9954] Created tests for EDB with EDB driver
    
    * [DAT-9954] Config file fix
    
    * [DAT-9954] Made requested fixes
    
    * [DAT-9954]Made distinguishing EDB with PostgreSQL and EDB drivers more clear
    
    * [DAT-9954]Made changes for github actions to run tests against both EDB with Postgres and EDB drivers
    
    * [DAT-9954]Fixed automation-runner
    
    * [DAT-9954]Fixed create-infra.sh and teardown-infra.sh
    
    * [DAT-9954]Fixed create-infra.sh and teardown-infra.sh
    
    * [DAT-9954]Fixed changeData tests
    
    * fixed setPrimaryKeyColumnRemarks
    
    Co-authored-by: KushnirykOleh <kushnirykoleh@gmail.com>
    
    * Added platform to mysql docker instances for compatability with Mac M1 machines
    
    * Revert "Added platform to mysql docker instances for compatability with Mac M1 machines"
    
    This reverts commit 1414399c895741bac2d54cc8666794030944c0cc.
    
    * Add platforms to ensure compatability with Mac M1 machines
    
    * paginate through results of workflow runs on client side
    
    * only paginate through a maximum of 10 pages
    
    * add missing values to returnData in workflow-helper.js (#215)
    
    * Updated jdbc driver versions to match new liquibase versions
    
    * Set trustServerCertificate=true in sqlserver connection URLs
    
    * Use hsqldb 2.5.2 since that is the latest version that supports java 8
    
    * Updated jaybird driver to 4.0.6
    
    * updated regex for datetime
    
    * Updated build logic to rely on liquibase-sdk-maven-plugin (#225)
    
    * Updated build logic to use liquibase-sdk-maven-plugin more heavily
    
    * Fall back to master build if branch is being computed
    
    * Corectly configure 0-SNAPSHOT liquibase in tests
    
    * Run 0-SNAPSHOT liquibase in tests in
    
    * Giving cockroachdb a bit more time to start
    
    * Log cockroach startup
    
    * Fixing build logic
    
    * [no-jira] pulled main, unignored sqlite after fix (#227)
    
    Co-authored-by: AlexanderSashchenko <santys87@gmail.com>
    
    * Added back deprecated liquibaseCommit input param
    
    * - Updated input description in main.yml
    - Put back workflow-helper.js findMatchingBranches for builds other than main.yml to use
    
    * updated assert to see failure reason in report
    
    * Updated liquibase-sdk-maven-plugin
    
    Co-authored-by: Nathan Voxland <nathan@voxland.net>
    Co-authored-by: yodzhubeiskyi <oesevg@gmail.com>
    Co-authored-by: Kevin Chappell <kevin-at-datical@users.noreply.github.com>
    Co-authored-by: Steven Massaro <steven.massaro.web@gmail.com>
    Co-authored-by: AlexanderSashchenko <santys87@gmail.com>

--- src/main/resources/liquibase/harness/change/expectedSql/sqlite/alterSequence.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/sqlite/alterSequence.sql
@@ -2,5 +2,5 @@ INVALID TEST
 
 SQLite does not support sequences.
 
--- https://docs.microsoft.com/en-us/ef/coreviders/sqlite/limitations
--- https://docs.liquibase.com/change-types/alter-sequence.html
+https://docs.microsoft.com/en-us/ef/coreviders/sqlite/limitations
+https://docs.liquibase.com/change-types/alter-sequence.html
--- src/main/resources/liquibase/harness/change/expectedSql/sqlite/enableTrigger.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/sqlite/enableTrigger.sql
@@ -2,4 +2,4 @@ INVALID TEST
 
 Liquibase does not support enableTrigger changeType for SQLite
 
--- See https://docs.liquibase.com/change-types/enable-trigger.html
+See https://docs.liquibase.com/change-types/enable-trigger.html
