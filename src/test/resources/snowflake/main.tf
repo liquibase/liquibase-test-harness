ed4b95701d549c06d74ea337abc7e76e5ac944f9
Jake Newton <jake@liquibase.com>
2022-06-07 14:30:26 -0500

    DAT-10450 snowflake action

--- src/test/resources/snowflake/main.tf
+++ src/test/resources/snowflake/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  cloud {
+    organization = "liquibase"
+    workspaces {
+      name = "test-harness-snowflake"
+    }
+  }
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
@@ -12,15 +18,56 @@ provider "snowflake" {
   role  = "SYSADMIN"
 }
 
+provider "snowflake" {
+  alias = "security_admin"
+  role  = "SECURITYADMIN"
+}
+
+resource "snowflake_role" "role" {
+  provider = snowflake.security_admin
+  name     = "TEST_HARNESS_SVC_ROLE"
+}
+
 resource "snowflake_database" "db" {
   provider = snowflake.sys_admin
   name     = "TEST_HARNESS"
 }
 
-# resource "snowflake_warehouse" "warehouse" {
-#   provider       = snowflake.sys_admin
-#   name           = "TEST_HARNESS"
-#   warehouse_size = "large"
+resource "snowflake_warehouse" "warehouse" {
+  provider       = snowflake.sys_admin
+  name           = "TEST_HARNESS"
+  warehouse_size = "x-small"
 
-#   auto_suspend = 60
-# }
+  auto_suspend = 60
+}
+
+resource "snowflake_database_grant" "grant" {
+  provider          = snowflake.security_admin
+  database_name     = snowflake_database.db.name
+  privilege         = "USAGE"
+  roles             = [snowflake_role.role.name]
+  with_grant_option = false
+}
+
+resource "snowflake_warehouse_grant" "grant" {
+  provider          = snowflake.security_admin
+  warehouse_name    = snowflake_warehouse.warehouse.name
+  privilege         = "USAGE"
+  roles             = [snowflake_role.role.name]
+  with_grant_option = false
+}
+
+resource "snowflake_user" "user" {
+  provider          = snowflake.security_admin
+  name              = "test_harness_user"
+  password          = "j^9wr+ccMB@6;%ky"
+  default_warehouse = snowflake_warehouse.warehouse.name
+  default_role      = snowflake_role.role.name
+  default_namespace = "${snowflake_database.db.name}.public"
+}
+
+resource "snowflake_role_grants" "grants" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_role.role.name
+  users     = [snowflake_user.user.name]
+}
