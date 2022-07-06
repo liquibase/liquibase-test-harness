701dee8425eca2105aba00e72892fb572c883dd8
ap-liquibase <86620405+ap-liquibase@users.noreply.github.com>
2022-07-06 10:21:34 -0400

    DAT-10549 fix format for azureSqlDbEndpoint output

--- src/test/resources/azure/outputs.tf
+++ src/test/resources/azure/outputs.tf
@@ -1,10 +1,8 @@
 
 # Output endpoint (fqdn)
 output "azureSqlDbEndpoint" {
-  value = {
-    description ="FQDN of mssql server created"
+    description = "FQDN of mssql server created"
     value       = azurerm_mssql_server.sql_server.fully_qualified_domain_name
-  }
 }
 
 output "admin_password" {
