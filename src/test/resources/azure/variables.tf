cfc30ab9362aa18888bda61f63bba19ecdc025e4
ap-liquibase <86620405+ap-liquibase@users.noreply.github.com>
2022-07-06 13:13:01 -0400

    DAT-10549 use pre-created resource group

--- src/test/resources/azure/variables.tf
+++ src/test/resources/azure/variables.tf
@@ -11,7 +11,7 @@ variable "location" {
 }
 
 variable "resource_group_name" {
-  default     = "TestHarness"
+  default     = "AzureTestHarness"
   description = "The resource group the sql server should be placed in. "
 }
 
