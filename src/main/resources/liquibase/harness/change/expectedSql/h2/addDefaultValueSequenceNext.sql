1430028856b0a8ee71b66626023327a8ad97f5cb
Nathan Voxland <nathan@voxland.net>
2022-07-28 16:44:28 -0500

    Update test for change in h2 from "number" to "numeric"

--- src/main/resources/liquibase/harness/change/expectedSql/h2/addDefaultValueSequenceNext.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/h2/addDefaultValueSequenceNext.sql
@@ -1,3 +1,3 @@
 CREATE SEQUENCE PUBLIC.test_sequence START WITH 1 INCREMENT BY 1 MINVALUE 1
-ALTER TABLE PUBLIC.authors ADD sequence_referenced_column NUMBER
+ALTER TABLE PUBLIC.authors ADD sequence_referenced_column numeric
 ALTER TABLE PUBLIC.authors ALTER COLUMN  sequence_referenced_column SET DEFAULT NEXTVAL('PUBLIC.test_sequence')
