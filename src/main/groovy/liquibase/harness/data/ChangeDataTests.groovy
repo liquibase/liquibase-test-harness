b96e70b3f5f8117017494e18b68a9aabe841c54e
AlexanderSashchenko <santys87@gmail.com>
2022-08-31 15:49:14 +0300

    fix for aws rds oracle/postgres

--- src/main/groovy/liquibase/harness/data/ChangeDataTests.groovy
+++ src/main/groovy/liquibase/harness/data/ChangeDataTests.groovy
@@ -97,10 +97,12 @@ class ChangeDataTests extends Specification {
                 newConnection = DriverManager.getConnection(testInput.url, testInput.username, testInput.password)
                 resultSet = newConnection.createStatement().executeQuery(checkingSql)
                 generatedResultSetArray = mapResultSetToJSONArray(resultSet)
+                newConnection.setAutoCommit(false)
                 newConnection.commit()
             } else {
                 resultSet = ((JdbcConnection) connection).createStatement().executeQuery(checkingSql)
                 generatedResultSetArray = mapResultSetToJSONArray(resultSet)
+                connection.setAutoCommit(false)
                 connection.commit()
             }
             def expectedResultSetJSON = new JSONObject(expectedResultSet)
