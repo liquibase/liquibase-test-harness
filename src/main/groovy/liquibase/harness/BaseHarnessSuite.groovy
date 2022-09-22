4a740d9458d01326640c1ad1a71421bde81bbaba
yodzhubeiskyi <oesevg@gmail.com>
2022-09-22 14:23:03 +0300

    AWS MySQL test

--- src/main/groovy/liquibase/harness/BaseHarnessSuite.groovy
+++ src/main/groovy/liquibase/harness/BaseHarnessSuite.groovy
@@ -9,8 +9,7 @@ import org.junit.runners.Suite
 @RunWith(Suite.class)
 @Suite.SuiteClasses([
 //        ChangeObjectTests,
-//        ChangeDataTests,
-        SnapshotObjectTests
+        ChangeDataTests, SnapshotObjectTests
 ])
 abstract class BaseHarnessSuite {
 }
