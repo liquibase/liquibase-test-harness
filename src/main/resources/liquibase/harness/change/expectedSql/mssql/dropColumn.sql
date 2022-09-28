57fcce21d6ee7a6925a72501d0c279588f5d2154
KushnirykOleh <kushnirykoleh@gmail.com>
2022-09-28 17:14:23 +0300

    unignored dropCoulmn for mssql

--- src/main/resources/liquibase/harness/change/expectedSql/mssql/dropColumn.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/mssql/dropColumn.sql
@@ -1,7 +1,3 @@
-INVALID TEST
--- Actually this test case has a bug and is marked INVALID in order to avoid ignoring whole platform at Github actions tests
--- https://github.com/liquibase/liquibase/issues/1818
-
 ALTER TABLE posts ADD varcharColumn varchar(25)
 UPDATE posts SET varcharColumn = 'INITIAL_VALUE'
 ALTER TABLE posts DROP COLUMN varcharColumn
