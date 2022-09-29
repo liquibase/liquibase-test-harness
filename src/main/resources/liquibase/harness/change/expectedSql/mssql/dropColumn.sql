79db896a75a00b66f835e6c259072d02585c8d3b
KushnirykOleh <kushnirykOleh@gmail.com>
2022-09-29 08:51:39 +0300

    [no-jira] unignored dropColumn for mssql (#365)
    * unignored dropCoulmn for mssql
    
    * removed extected sql
    
    * returned expectedSql with drop constraints rows

--- src/main/resources/liquibase/harness/change/expectedSql/mssql/dropColumn.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/mssql/dropColumn.sql
@@ -1,7 +1,6 @@
-INVALID TEST
--- Actually this test case has a bug and is marked INVALID in order to avoid ignoring whole platform at Github actions tests
--- https://github.com/liquibase/liquibase/issues/1818
-
 ALTER TABLE posts ADD varcharColumn varchar(25)
 UPDATE posts SET varcharColumn = 'INITIAL_VALUE'
+DECLARE @sql [nvarchar](MAX)
+SELECT @sql = N'ALTER TABLE posts DROP CONSTRAINT ' + QUOTENAME([df].[name]) FROM [sys].[columns] AS [c] INNER JOIN [sys].[default_constraints] AS [df] ON [df].[object_id] = [c].[default_object_id] WHERE [c].[object_id] = OBJECT_ID(N'posts') AND [c].[name] = N'varcharColumn'
+    EXEC sp_executesql @sql
 ALTER TABLE posts DROP COLUMN varcharColumn
