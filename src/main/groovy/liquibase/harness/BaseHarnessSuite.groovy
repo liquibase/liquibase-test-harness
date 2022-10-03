fa8758dac8ce1f03f0e77cf60b7ecb20a79aafdc
AlexanderSashchenko <santys87@gmail.com>
2022-10-03 16:31:43 +0300

    fixes after review

--- src/main/groovy/liquibase/harness/BaseHarnessSuite.groovy
+++ src/main/groovy/liquibase/harness/BaseHarnessSuite.groovy
@@ -1,6 +1,5 @@
 package liquibase.harness
 
-import liquibase.harness.base.BaseCompatibilityTest
 import liquibase.harness.change.ChangeObjectTests
 import liquibase.harness.data.ChangeDataTests
 import liquibase.harness.snapshot.SnapshotObjectTests
@@ -8,6 +7,6 @@ import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses([BaseCompatibilityTest])
+@Suite.SuiteClasses([ChangeObjectTests, ChangeDataTests, SnapshotObjectTests])
 abstract class BaseHarnessSuite {
 }
