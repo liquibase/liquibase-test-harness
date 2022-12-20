5a60f94734a99665066b16109a43bcece0b0fc6f
yodzhubeiskyi <oesevg@gmail.com>
2022-12-20 13:10:21 +0200

    Bug fix

--- src/main/groovy/liquibase/harness/BaseHarnessSuite.groovy
+++ src/main/groovy/liquibase/harness/BaseHarnessSuite.groovy
@@ -2,7 +2,6 @@ package liquibase.harness
 
 import liquibase.harness.change.ChangeObjectTests
 import liquibase.harness.data.ChangeDataTests
-import liquibase.harness.snapshot.SnapshotObjectTests
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
--- src/test/groovy/liquibase/harness/CompatibilityTest.groovy
+++ src/test/groovy/liquibase/harness/CompatibilityTest.groovy
@@ -3,7 +3,7 @@ package liquibase.harness
 
 import liquibase.harness.config.TestConfig
 
-class CompatibilityTest extends liquibase.harness.compatibility.basic.CompatibilityTest {
+class CompatibilityTest extends liquibase.harness.compatibility.foundational.FoundationalTest {
 
     static {
         //extensions don't distribute their files. Only we store them in src/main/resources.
