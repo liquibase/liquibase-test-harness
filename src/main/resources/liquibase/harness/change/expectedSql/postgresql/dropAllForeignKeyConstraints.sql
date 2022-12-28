79191008774c5b7bc77b4771fad72d378badac97
Daniel Mallorga <dmallorga@liquibase.com>
2022-12-28 20:02:34 -0300

    Updated expectd dropAllForeignKeyConstraints sql file

--- src/main/resources/liquibase/harness/change/expectedSql/postgresql/dropAllForeignKeyConstraints.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/postgresql/dropAllForeignKeyConstraints.sql
@@ -1,2 +1,3 @@
 ALTER TABLE public.posts ADD CONSTRAINT fk_posts_authors_test_1 FOREIGN KEY (author_id) REFERENCES public.authors (id)
 ALTER TABLE public.posts ADD CONSTRAINT fk_posts_authors_test_2 FOREIGN KEY (id) REFERENCES public.authors (id)
+ALTER TABLE public.posts DROP CONSTRAINT fk_posts_authors_test
