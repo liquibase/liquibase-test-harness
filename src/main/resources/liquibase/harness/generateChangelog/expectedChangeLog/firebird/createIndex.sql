831bc862bb133c523007cb8062813c08dcf1296c
yodzhubeiskyi <oesevg@gmail.com>
2023-01-24 12:36:37 +0100

    Firebird small fix

--- src/main/resources/liquibase/harness/generateChangelog/expectedChangeLog/firebird/createIndex.sql
+++ src/main/resources/liquibase/harness/generateChangelog/expectedChangeLog/firebird/createIndex.sql
@@ -1,3 +1,4 @@
 INVALID TEST
 
 --Indexes are treated as unique constraints during generateChangelog command for Firebird
+--Change after https://datical.atlassian.net/browse/DAT-13129 issue is resolved
