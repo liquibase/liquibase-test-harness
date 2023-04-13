a51e99916b0526c5a681fd2ffa6bebe1a7bf4992
jandroav <jandroav@icloud.com>
2023-04-13 15:12:06 +0200

    refactor(mssql.sql): add conditional statements to check if user and role already exist before creating them

--- src/test/resources/init-changelogs/gcp/mssql.sql
+++ src/test/resources/init-changelogs/gcp/mssql.sql
@@ -1,8 +1,14 @@
 --liquibase formatted sql
---changeset liquibase:1 runAlways:true failOnError:false
+--changeset liquibase:1 runAlways:true
 
 USE lbcat;
 
-EXEC sp_adduser 'lbuser';
+IF NOT EXISTS (SELECT * FROM sys.sysusers WHERE name = 'lbuser')
+BEGIN
+    EXEC sp_adduser 'lbuser';
+END
 
-EXEC sp_addrolemember 'db_owner', 'lbuser'
+IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'lbuser' AND type_desc = 'DATABASE_ROLE' AND is_fixed_role = 0)
+BEGIN
+    EXEC sp_addrolemember 'db_owner', 'lbuser';
+END
