64ef3d79fbd5774f6c421e45d991e459c37802e9
Pavlo Tytarchuk <ptyta@softserveinc.com>
2023-04-20 11:59:04 +0200

    DAT-14478. refactored AdvancedHarnessSuiteTest to use older version of advanced test

--- src/main/groovy/liquibase/harness/AdvancedHarnessSuite.groovy
+++ src/main/groovy/liquibase/harness/AdvancedHarnessSuite.groovy
@@ -1,10 +1,12 @@
 package liquibase.harness
 
 import liquibase.harness.compatibility.advanced.AdvancedTest
+import liquibase.harness.generateChangelog.GenerateChangelogTest
+import liquibase.harness.snapshot.SnapshotObjectTests
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses([AdvancedTest])
+@Suite.SuiteClasses([GenerateChangelogTest, SnapshotObjectTests])
 abstract class AdvancedHarnessSuite {
 }
