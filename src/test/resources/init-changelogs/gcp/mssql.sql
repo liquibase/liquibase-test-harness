9b13a71a268935100c8e99dafdba6a52ae9c572a
Pavlo Tytarchuk <ptyta@softserveinc.com>
2023-04-27 15:09:38 +0200

    DAT-14556. changes init script schema to dbo from lbuser for gcp mssql

--- src/test/resources/init-changelogs/gcp/mssql.sql
+++ src/test/resources/init-changelogs/gcp/mssql.sql
@@ -1,9 +1,9 @@
 --liquibase formatted sql
 --changeset liquibase:1 runAlways:true
 
-DROP TABLE IF EXISTS [lbuser].[authors]
+DROP TABLE IF EXISTS [dbo].[authors]
 
-CREATE TABLE [lbuser].[authors](
+CREATE TABLE [dbo].[authors](
     [id] [int] NOT NULL,
     [first_name] [varchar](255) NOT NULL,
     [last_name] [varchar](255) NULL,
@@ -13,16 +13,16 @@ CREATE TABLE [lbuser].[authors](
     CONSTRAINT [PK_PrimaryKey] PRIMARY KEY ([id])
     ) ON [PRIMARY]
 
-    INSERT INTO [lbuser].[authors] ([id],[first_name],[last_name],[email],[birthdate],[added])
+    INSERT INTO [dbo].[authors] ([id],[first_name],[last_name],[email],[birthdate],[added])
     VALUES ('1','Eileen','Lubowitz','ppaucek@example.org','1991-03-04','2004-05-30 02:08:25'),
 ('2','Tamia','Mayert','shansen@example.org','2016-03-27','2014-03-21 02:52:00'),
 ('3','Cyril','Funk','reynolds.godfrey@example.com','1988-04-21','2011-06-24 18:17:48'),
 ('4','Nicolas','Buckridge','xhoeger@example.net','2017-02-03','2019-04-22 02:04:41'),
 ('5','Jayden','Walter','lillian66@example.com','2010-02-27','1990-02-04 02:32:00')
 
-DROP TABLE IF EXISTS [lbuser].[posts]
+DROP TABLE IF EXISTS [dbo].[posts]
 
-CREATE TABLE [lbuser].[posts](
+CREATE TABLE [dbo].[posts](
     [id] [int] NOT NULL,
     [author_id] [int] NOT NULL,
     [title] [varchar](255) NULL,
@@ -31,7 +31,7 @@ CREATE TABLE [lbuser].[posts](
     [inserted_date] [date] NULL
     ) ON [PRIMARY]
 
-    INSERT INTO [lbuser].[posts] ([id],[author_id],[title],[description],[content],[inserted_date])
+    INSERT INTO [dbo].[posts] ([id],[author_id],[title],[description],[content],[inserted_date])
     VALUES ('1','1','temporibus','voluptatum','Fugit non et doloribus repudiandae.','2015-11-18'),
 ('2','2','ea','aut','Tempora molestias maiores provident molestiae sint possimus quasi.','1975-06-08'),
 ('3','3','illum','rerum','Delectus recusandae sit officiis dolor.','1975-02-25'),
