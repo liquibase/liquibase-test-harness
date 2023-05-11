79ca41bd30a80698fe7f91450f66bc6bda124b3b
KushnirykOleh <kushnirykoleh@gmail.com>
2023-05-11 15:58:16 +0300

    override sqqlFile for different oracle instances

--- src/main/resources/liquibase/harness/change/expectedSql/oracle/aws_19/sqlFile.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/oracle/aws_19/sqlFile.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "C##LIQUIBASE".sqltest (id INTEGER)
+CREATE TABLE LBUSER.sqltest (id INTEGER)
 insert into sqltest (id) values (1)
 insert into sqltest (id) values (2)
 insert into sqltest (id) values (3)
