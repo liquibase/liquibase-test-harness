e9e4fcb9c450547ad1acf494f5e38b797cce01eb
Pavlo Tytarchuk <ptyta@softserveinc.com>
2023-05-26 15:26:44 +0200

    DAT-14657. advanced test debug.

--- src/main/groovy/liquibase/harness/AdvancedHarnessSuite.groovy
+++ src/main/groovy/liquibase/harness/AdvancedHarnessSuite.groovy
@@ -1,12 +1,12 @@
 package liquibase.harness
 
-
+import liquibase.harness.compatibility.advanced.AdvancedTest
 import liquibase.harness.generateChangelog.GenerateChangelogTest
 import liquibase.harness.snapshot.SnapshotObjectTests
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses([GenerateChangelogTest, SnapshotObjectTests])
+@Suite.SuiteClasses([AdvancedTest])
 abstract class AdvancedHarnessSuite {
 }
