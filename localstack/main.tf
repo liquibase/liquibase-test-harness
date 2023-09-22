a27b7a02e34dc73022d38b65ef0a1793d4740394
jandroav <jandroav@icloud.com>
2023-09-22 13:51:46 +0200

    chore(localstack): update main.tf to use required_providers block for better provider management chore(localstack): remove versions.tf file as it is no longer needed for provider versions

--- localstack/main.tf
+++ localstack/main.tf
@@ -1,3 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "5.37.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "github" {
+  owner = "liquibase"
+}
+
 module "test-automation" {
   source  = "app.terraform.io/liquibase/test-automation/aws"
   version = "0.1.0"
