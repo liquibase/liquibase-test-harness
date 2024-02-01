b596fe38006367321ee09aaeb3f8fd14e20506bb
jandroav <jandroav@icloud.com>
2024-02-01 15:01:21 +0100

    fix(liquibase): change table and sequence names to use lowercase "public" schema for consistency and improve readability
    The table and sequence names in the `createPackage.xml`, `createPackageBody.xml`, `alterSequence.sql`, `createPackage.sql`, `createPackageBody.sql`, `createSequence.sql`, `dropFunction.sql`, `dropSequence.sql`, `renameSequence.sql`, and `valueSequenceNext.sql` files were changed to use the lowercase "public" schema instead of the uppercase "PUBLIC" schema. This change was made to improve consistency and readability in the Liquibase change scripts.

--- src/main/resources/liquibase/harness/change/expectedSql/snowflake/valueSequenceNext.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/snowflake/valueSequenceNext.sql
@@ -1,3 +1,3 @@
 CREATE TABLE "public".valueSequenceNextTable (test_id INT, test_column VARCHAR(50));
-CREATE SEQUENCE "PUBLIC".test_sequence START WITH 30 INCREMENT BY 2;
-UPDATE "public".valueSequenceNextTable SET test_id = "PUBLIC".test_sequence.nextval;
+CREATE SEQUENCE "public".test_sequence START WITH 30 INCREMENT BY 2;
+UPDATE "public".valueSequenceNextTable SET test_id = "public".test_sequence.nextval;
