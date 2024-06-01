b173feb8e6d991d09675f8771eda947749665ceb
jandroav <jandroav@icloud.com>
2024-06-01 21:15:47 +0200

    ✨ (aws-weekly.yml): Add a new line to improve readability and maintain consistency in the workflow file 📝 (mssql.sql): Add database creation and switching commands for better database management and setup

--- src/test/resources/init-changelogs/aws/mssql.sql
+++ src/test/resources/init-changelogs/aws/mssql.sql
@@ -1,5 +1,9 @@
 --liquibase formatted sql
 --changeset liquibase:1 runAlways:true
+USE master;
+CREATE DATABASE lbcat;
+GO
+USE NewDB;
 DROP TABLE IF EXISTS [dbo].[authors]
 CREATE TABLE [dbo].[authors](
     [id] [int] NOT NULL,
