f5416f586e340224142342a92e57cde017fca86c
Vitalii Makarchenkov <mail@vitaliimak.com>
2024-07-02 16:24:34 +0300

    Fix tests for 3.16.0 driver version

--- src/main/resources/liquibase/harness/change/expectedSql/snowflake/dropAllForeignKeyConstraints.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/snowflake/dropAllForeignKeyConstraints.sql
@@ -1,2 +1,2 @@
-ALTER TABLE LTHDB."PUBLIC".posts ADD CONSTRAINT fk_posts_authors_test_1 FOREIGN KEY (author_id) REFERENCES LTHDB."PUBLIC".authors (id)
-ALTER TABLE LTHDB."PUBLIC".posts ADD CONSTRAINT fk_posts_authors_test_2 FOREIGN KEY (id) REFERENCES LTHDB."PUBLIC".authors (id)
+ALTER TABLE LTHDB.PUBLIC.posts ADD CONSTRAINT fk_posts_authors_test_1 FOREIGN KEY (author_id) REFERENCES LTHDB.PUBLIC.authors (id)
+ALTER TABLE LTHDB.PUBLIC.posts ADD CONSTRAINT fk_posts_authors_test_2 FOREIGN KEY (id) REFERENCES LTHDB.PUBLIC.authors (id)
