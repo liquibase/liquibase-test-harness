aad29a69f56ed12294969960895bc9c7b0825ad5
Vitalii Makarchenkov <mail@vitaliimak.com>
2024-07-02 17:38:48 +0300

    [DAT-17590] Fix tests for 3.16.0 driver version (#833)
    Fix tests for 3.16.0 driver version

--- src/main/resources/liquibase/harness/change/expectedSql/snowflake/dropForeignKey.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/snowflake/dropForeignKey.sql
@@ -1,2 +1,2 @@
-ALTER TABLE LTHDB."PUBLIC".posts ADD CONSTRAINT fk_posts_authors_test FOREIGN KEY (author_id) REFERENCES LTHDB."PUBLIC".authors (id)
-ALTER TABLE LTHDB."PUBLIC".posts DROP CONSTRAINT fk_posts_authors_test
+ALTER TABLE LTHDB.PUBLIC.posts ADD CONSTRAINT fk_posts_authors_test FOREIGN KEY (author_id) REFERENCES LTHDB.PUBLIC.authors (id)
+ALTER TABLE LTHDB.PUBLIC.posts DROP CONSTRAINT fk_posts_authors_test
--- src/main/resources/liquibase/harness/change/expectedSql/snowflake/mergeColumns.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/snowflake/mergeColumns.sql
@@ -1,9 +1,9 @@
-CREATE TABLE LTHDB."PUBLIC".full_name_table (first_name VARCHAR(50), last_name VARCHAR(50))
-INSERT INTO LTHDB."PUBLIC".full_name_table (first_name) VALUES ('John')
-UPDATE LTHDB."PUBLIC".full_name_table SET last_name = 'Doe' WHERE first_name='John'
-INSERT INTO LTHDB."PUBLIC".full_name_table (first_name) VALUES ('Jane')
-UPDATE LTHDB."PUBLIC".full_name_table SET last_name = 'Doe' WHERE first_name='Jane'
-ALTER TABLE LTHDB."PUBLIC".full_name_table ADD full_name VARCHAR(255)
-UPDATE LTHDB."PUBLIC".full_name_table SET full_name = first_name || ' ' || last_name
-ALTER TABLE LTHDB."PUBLIC".full_name_table DROP COLUMN first_name
-ALTER TABLE LTHDB."PUBLIC".full_name_table DROP COLUMN last_name
+CREATE TABLE LTHDB.PUBLIC.full_name_table (first_name VARCHAR(50), last_name VARCHAR(50))
+INSERT INTO LTHDB.PUBLIC.full_name_table (first_name) VALUES ('John')
+UPDATE LTHDB.PUBLIC.full_name_table SET last_name = 'Doe' WHERE first_name='John'
+INSERT INTO LTHDB.PUBLIC.full_name_table (first_name) VALUES ('Jane')
+UPDATE LTHDB.PUBLIC.full_name_table SET last_name = 'Doe' WHERE first_name='Jane'
+ALTER TABLE LTHDB.PUBLIC.full_name_table ADD full_name VARCHAR(255)
+UPDATE LTHDB.PUBLIC.full_name_table SET full_name = first_name || ' ' || last_name
+ALTER TABLE LTHDB.PUBLIC.full_name_table DROP COLUMN first_name
+ALTER TABLE LTHDB.PUBLIC.full_name_table DROP COLUMN last_name
