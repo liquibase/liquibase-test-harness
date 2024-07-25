f7eaa573b52911ffe7561a72a056c3ce3e7940c5
Pavlo Tytarchuk <ptyta@softserveinc.com>
2024-07-25 15:00:02 +0200

    added new path to sqlcmd for mssql-2022-latest image

--- src/test/resources/docker/mssql-init-2022.sh
+++ src/test/resources/docker/mssql-init-2022.sh
@@ -6,7 +6,7 @@ echo "Waiting for server to start...."
 #do this in a loop because the timing for when the SQL instance is ready is indeterminate
 for i in {1..50};
 do
-    /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -d master -i /docker-entrypoint-initdb.d/mssql-init.sql
+    /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -C -d master -i /docker-entrypoint-initdb.d/mssql-init.sql
     if [ $? -eq 0 ]
     then
         echo "mssql-init.sh completed"
