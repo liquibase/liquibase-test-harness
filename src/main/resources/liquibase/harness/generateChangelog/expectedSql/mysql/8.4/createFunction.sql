5d09b1c9f562efc9d9022d69b1239322e6822ef7
Taras Amelianovych <97164517+Tamelianovych@users.noreply.github.com>
2025-01-13 13:07:47 +0100

    Fixing mysql 8.4 advanced tests (#966)

---
+++ src/main/resources/liquibase/harness/generateChangelog/expectedSql/mysql/8.4/createFunction.sql
@@ -0,0 +1,5 @@
+CREATE FUNCTION `test_function`() RETURNS varchar(20) CHARSET utf8mb4
+BEGIN
+                                                RETURN 'Hello';
+                                                END;
+
