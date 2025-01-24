ac8f77a27d6b1417ce08f6c19e047a15bd200fbd
Tamelianovych <amelianovychtaras@gmail.com>
2025-01-24 13:48:33 +0100

    DAT-12910 uncommented code ;)

--- src/main/groovy/liquibase/harness/diff/DiffCommandTest.groovy
+++ src/main/groovy/liquibase/harness/diff/DiffCommandTest.groovy
@@ -116,41 +116,41 @@ class DiffCommandTest extends Specification {
         testInput << buildTestInput()
     }
 
-//    @Unroll
-//    def "apply diff test against #testInput.referenceDatabase.name #testInput.referenceDatabase.version to targetDatabase #testInput.targetDatabase.name #testInput.targetDatabase.version"() {
-//        given: "read input data for diff test"
-//        Map<String, Object> argsMap = new HashMap()
-//        argsMap.put("url", testInput.targetDatabase.url)
-//        argsMap.put("username", testInput.targetDatabase.username)
-//        argsMap.put("password", testInput.targetDatabase.password)
-//        argsMap.put("referenceUrl", testInput.referenceDatabase.url)
-//        argsMap.put("referenceUsername", testInput.referenceDatabase.username)
-//        argsMap.put("referencePassword", testInput.referenceDatabase.password)
-//        argsMap.put("changelogFile", testInput.pathToChangelogFile)
-//
-//        and: "check databases are online"
-//        assert testInput.targetDatabase.database.getConnection() instanceof JdbcConnection: "Target database " +
-//                "${testInput.targetDatabase.name}${testInput.targetDatabase.version} is offline!"
-//        assert testInput.referenceDatabase.database.getConnection() instanceof JdbcConnection: "Reference database " +
-//                "${testInput.referenceDatabase.name}${testInput.referenceDatabase.version} is offline!"
-//
-//        when: "execute update and diff commands"
-//
-//        executeCommandScope("update", argsMap)
-//        argsMap.put("excludeObjects", "(?i)posts, (?i)authors, (?i)databasechangelog, (?i)databasechangeloglock")
-//        String generatedDiffContent = removeDatabaseInfoFromDiff(executeCommandScope("diff", argsMap).toString())
-//
-//        then: "validate generated diff"
-//        String expectedDiffContent = removeDatabaseInfoFromDiff(getResourceContent(testInput.pathToExpectedDiffFile))
-//        assert expectedDiffContent == generatedDiffContent
-//
-//        cleanup: "rollback changes"
-//        strategy.performRollback(argsMap)
-//
-//        where:
-//        testInput << buildTestInput()
-//    }
-//    def cleanupSpec() {
-//        strategy.cleanupDatabase(databases)
-//    }
+    @Unroll
+    def "apply diff test against #testInput.referenceDatabase.name #testInput.referenceDatabase.version to targetDatabase #testInput.targetDatabase.name #testInput.targetDatabase.version"() {
+        given: "read input data for diff test"
+        Map<String, Object> argsMap = new HashMap()
+        argsMap.put("url", testInput.targetDatabase.url)
+        argsMap.put("username", testInput.targetDatabase.username)
+        argsMap.put("password", testInput.targetDatabase.password)
+        argsMap.put("referenceUrl", testInput.referenceDatabase.url)
+        argsMap.put("referenceUsername", testInput.referenceDatabase.username)
+        argsMap.put("referencePassword", testInput.referenceDatabase.password)
+        argsMap.put("changelogFile", testInput.pathToChangelogFile)
+
+        and: "check databases are online"
+        assert testInput.targetDatabase.database.getConnection() instanceof JdbcConnection: "Target database " +
+                "${testInput.targetDatabase.name}${testInput.targetDatabase.version} is offline!"
+        assert testInput.referenceDatabase.database.getConnection() instanceof JdbcConnection: "Reference database " +
+                "${testInput.referenceDatabase.name}${testInput.referenceDatabase.version} is offline!"
+
+        when: "execute update and diff commands"
+
+        executeCommandScope("update", argsMap)
+        argsMap.put("excludeObjects", "(?i)posts, (?i)authors, (?i)databasechangelog, (?i)databasechangeloglock")
+        String generatedDiffContent = removeDatabaseInfoFromDiff(executeCommandScope("diff", argsMap).toString())
+
+        then: "validate generated diff"
+        String expectedDiffContent = removeDatabaseInfoFromDiff(getResourceContent(testInput.pathToExpectedDiffFile))
+        assert expectedDiffContent == generatedDiffContent
+
+        cleanup: "rollback changes"
+        strategy.performRollback(argsMap)
+
+        where:
+        testInput << buildTestInput()
+    }
+    def cleanupSpec() {
+        strategy.cleanupDatabase(databases)
+    }
 }
