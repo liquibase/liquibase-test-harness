db61b4316e8d3145ec62fee647675ec8ac6caac4
Pavlo Tytarchuk <ptyta@softserveinc.com>
2025-01-24 12:59:32 +0100

    DAT-19460. using @ignore instead of commenting

--- src/main/groovy/liquibase/harness/stress/StressTest.groovy
+++ src/main/groovy/liquibase/harness/stress/StressTest.groovy
@@ -6,6 +6,7 @@ import liquibase.harness.config.DatabaseUnderTest
 import liquibase.harness.config.TestConfig
 import liquibase.harness.util.rollback.RollbackStrategy
 import liquibase.ui.UIService
+import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -13,6 +14,7 @@ import spock.lang.Unroll
 import static liquibase.harness.util.TestUtils.chooseRollbackStrategy
 import static liquibase.harness.util.TestUtils.executeCommandScope
 
+@Ignore("ignored until discussion regarding it's necessity")
 class StressTest extends Specification {
     @Shared
     RollbackStrategy strategy
@@ -20,56 +22,56 @@ class StressTest extends Specification {
     List<DatabaseUnderTest> databases
     @Shared
     UIService uiService = Scope.getCurrentScope().getUI()
-//
-//    long timeMillisBeforeTest
-//    long timeMillisAfterTest
-//
-//    def setupSpec() {
-//        databases = TestConfig.instance.getFilteredDatabasesUnderTest()
-//        strategy = chooseRollbackStrategy()
-//        strategy.prepareForRollback(databases)
-//    }
-//
-//    @Unroll
-//    def "apply stress test against #testInput.databaseName #testInput.version"() {
-//        given: "read input data for stress testing"
-//        Map<String, Object> argsMap = new HashMap()
-//        argsMap.put("url", testInput.url)
-//        argsMap.put("username", testInput.username)
-//        argsMap.put("password", testInput.password)
-//        boolean shouldRunChangeSet
-//
-//        and: "check database under test is online"
-//        def connection = testInput.database.getConnection()
-//        shouldRunChangeSet = connection instanceof JdbcConnection
-//        assert shouldRunChangeSet: "Database ${testInput.databaseName} ${testInput.version} is offline!"
-//
-//        and: "executing stress test with queries for 10000 rows"
-//        def map = new LinkedHashMap<String, String>()
-//        map.put("setup", testInput.setupChangelogPath)
-//        map.put("insert", testInput.insertChangelogPath)
-//        map.put("update", testInput.updateChangelogPath)
-//        map.put("select", testInput.selectChangelogPath)
-//        for (Map.Entry<String, String> entry : map.entrySet()) {
-//            timeMillisBeforeTest = System.currentTimeMillis()
-//            uiService.sendMessage("Executing $entry.key query: 10000 rows!")
-//            argsMap.put("changeLogFile", entry.value)
-//            executeCommandScope("update", argsMap)
-//            timeMillisAfterTest = System.currentTimeMillis()
-//            uiService.sendMessage("Execution time for $entry.key query: " + (timeMillisAfterTest - timeMillisBeforeTest) / 1000 + "s")
-//        }
-//
-//        cleanup: "rollback changes if we ran changeSet"
-//        if (shouldRunChangeSet) {
-//            argsMap.put("changeLogFile", testInput.setupChangelogPath)
-//            strategy.performRollback(argsMap)
-//        }
-//
-//        where: "test input in next data table"
-//        testInput << StressTestHelper.buildTestInput()
-//    }
-//
-//    def cleanupSpec() {
-//        strategy.cleanupDatabase(databases)
-//    }
+
+    long timeMillisBeforeTest
+    long timeMillisAfterTest
+
+    def setupSpec() {
+        databases = TestConfig.instance.getFilteredDatabasesUnderTest()
+        strategy = chooseRollbackStrategy()
+        strategy.prepareForRollback(databases)
+    }
+
+    @Unroll
+    def "apply stress test against #testInput.databaseName #testInput.version"() {
+        given: "read input data for stress testing"
+        Map<String, Object> argsMap = new HashMap()
+        argsMap.put("url", testInput.url)
+        argsMap.put("username", testInput.username)
+        argsMap.put("password", testInput.password)
+        boolean shouldRunChangeSet
+
+        and: "check database under test is online"
+        def connection = testInput.database.getConnection()
+        shouldRunChangeSet = connection instanceof JdbcConnection
+        assert shouldRunChangeSet: "Database ${testInput.databaseName} ${testInput.version} is offline!"
+
+        and: "executing stress test with queries for 10000 rows"
+        def map = new LinkedHashMap<String, String>()
+        map.put("setup", testInput.setupChangelogPath)
+        map.put("insert", testInput.insertChangelogPath)
+        map.put("update", testInput.updateChangelogPath)
+        map.put("select", testInput.selectChangelogPath)
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            timeMillisBeforeTest = System.currentTimeMillis()
+            uiService.sendMessage("Executing $entry.key query: 10000 rows!")
+            argsMap.put("changeLogFile", entry.value)
+            executeCommandScope("update", argsMap)
+            timeMillisAfterTest = System.currentTimeMillis()
+            uiService.sendMessage("Execution time for $entry.key query: " + (timeMillisAfterTest - timeMillisBeforeTest) / 1000 + "s")
+        }
+
+        cleanup: "rollback changes if we ran changeSet"
+        if (shouldRunChangeSet) {
+            argsMap.put("changeLogFile", testInput.setupChangelogPath)
+            strategy.performRollback(argsMap)
+        }
+
+        where: "test input in next data table"
+        testInput << StressTestHelper.buildTestInput()
+    }
+
+    def cleanupSpec() {
+        strategy.cleanupDatabase(databases)
+    }
 }
