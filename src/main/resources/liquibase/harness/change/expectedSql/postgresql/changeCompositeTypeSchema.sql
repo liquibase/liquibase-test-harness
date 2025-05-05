d82678b8ec37064dbe2f7ee9f4a6a26a257beef9
Charles Queiroz <fcqueiroz@liquibase.com>
2025-05-05 14:06:57 +0100

    INT-1268 - Add test harness support for Composite Type schema change in PostgreSQL

--- src/main/resources/liquibase/harness/change/expectedSql/postgresql/changeCompositeTypeSchema.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/postgresql/changeCompositeTypeSchema.sql
@@ -1,3 +1,3 @@
-CREATE TYPE public."myType" AS (attr1 integer, attr2 text COLLATE "en_US");
+CREATE TYPE public."myTypeSchema" AS (attr1 integer, attr2 text COLLATE "en_US");
 CREATE SCHEMA IF NOT EXISTS test_schema;
-ALTER TYPE public."myType" SET SCHEMA test_schema;
+ALTER TYPE public."myTypeSchema" SET SCHEMA test_schema;
