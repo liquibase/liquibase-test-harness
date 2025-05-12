24c46ab6ecfd7e6f33dd1b990abd25f778cf71a0
Mykhailo Savchenko <msavchenko@liquibase.com>
2025-05-12 17:13:22 +0300

    INT-1281: case of attr4 fixed both in create and rename sql-s

--- src/main/resources/liquibase/harness/change/expectedSql/postgresql/renameCompositeTypeAttribute.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/postgresql/renameCompositeTypeAttribute.sql
@@ -1,5 +1,5 @@
-CREATE TYPE public."myType" AS (attr1 int, attr2 text COLLATE "en_US", "Attr3" real, attr4 line)
+CREATE TYPE public."myType" AS (attr1 int, attr2 text COLLATE "en_US", "Attr3" real, ATTR4 line)
 ALTER TYPE public."myType" RENAME ATTRIBUTE attr1 TO "att®1" CASCADE
 ALTER TYPE public."myType" RENAME ATTRIBUTE attr2 TO "attribute 2" CASCADE
 ALTER TYPE public."myType" RENAME ATTRIBUTE "Attr3" TO "AtTrIbUtE3" CASCADE
-ALTER TYPE public."myType" RENAME ATTRIBUTE ATTR4 TO "attr & 4" CASCADE
+ALTER TYPE public."myType" RENAME ATTRIBUTE attr4 TO "attr & 4" CASCADE
