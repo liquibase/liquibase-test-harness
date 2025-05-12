a9eb361b106bcec69130ae904cf95a74cbfe70f7
Mykhailo Savchenko <msavchenko@liquibase.com>
2025-05-12 17:42:54 +0300

    INT-1281: regenerated expected SQL-s, alligned case of attribute names for renameAttribute test, extended createCompositeType test

--- src/main/resources/liquibase/harness/change/expectedSql/postgresql/renameCompositeTypeAttribute.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/postgresql/renameCompositeTypeAttribute.sql
@@ -2,4 +2,4 @@ CREATE TYPE public."myType" AS (attr1 int, attr2 text COLLATE "en_US", "Attr3" r
 ALTER TYPE public."myType" RENAME ATTRIBUTE attr1 TO "att®1" CASCADE
 ALTER TYPE public."myType" RENAME ATTRIBUTE attr2 TO "attribute 2" CASCADE
 ALTER TYPE public."myType" RENAME ATTRIBUTE "Attr3" TO "AtTrIbUtE3" CASCADE
-ALTER TYPE public."myType" RENAME ATTRIBUTE attr4 TO "attr & 4" CASCADE
+ALTER TYPE public."myType" RENAME ATTRIBUTE ATTR4 TO "attr & 4" CASCADE
