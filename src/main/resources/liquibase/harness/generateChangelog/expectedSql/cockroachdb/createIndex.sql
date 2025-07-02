2504d4f1edeba12b4ba049bd749e641f3b9cfc20
Pavlo Tytarchuk <ptyta@softserveinc.com>
2025-07-02 13:30:59 +0200

    INT-139. fixing advanced test failures

--- src/main/resources/liquibase/harness/generateChangelog/expectedSql/cockroachdb/createIndex.sql
+++ src/main/resources/liquibase/harness/generateChangelog/expectedSql/cockroachdb/createIndex.sql
@@ -1,3 +1,3 @@
 CREATE TABLE "test_table" ("id" BIGINT, "rowid" BIGINT DEFAULT unique_rowid() NOT NULL, CONSTRAINT "test_table_pkey" PRIMARY KEY ("rowid"));
 
-CREATE INDEX "idx_first_name" ON "test_table"("id");
+CREATE INDEX "idx_first_name" ON "test_table" USING prefix("id");
