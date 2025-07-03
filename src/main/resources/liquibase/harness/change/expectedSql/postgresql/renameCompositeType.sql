caf4a149dc548aa9f89afcb6ede71576a3953ac8
Pavlo Tytarchuk <ptyta@softserveinc.com>
2025-07-03 12:53:59 +0200

    INT-139. updated expected sql for postgres tests

--- src/main/resources/liquibase/harness/change/expectedSql/postgresql/renameCompositeType.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/postgresql/renameCompositeType.sql
@@ -1,2 +1,3 @@
+DROP TYPE IF EXISTS public."myType" CASCADE
 CREATE TYPE public."myRenameType" AS (attr1 int, attr2 text COLLATE "en-US-x-icu")
 ALTER TYPE public."myRenameType" RENAME TO "myNewNameType"
