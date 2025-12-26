74edc71f2dd34f40db78f59d977903b83d77735c
KushnirykOleh <kushnirykoleh@gmail.com>
2025-12-26 20:08:29 +0200

    fix valueSequenceNext.sql

--- src/main/resources/liquibase/harness/change/expectedSql/snowflake/valueSequenceNext.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/snowflake/valueSequenceNext.sql
@@ -1,3 +1,3 @@
 CREATE TABLE ${CATALOG_NAME}.PUBLIC.valueSequenceNextTable (test_id INT, test_column VARCHAR(50));
 CREATE SEQUENCE ${CATALOG_NAME}.PUBLIC.test_sequence START WITH 30 INCREMENT BY 2;
-UPDATE ${CATALOG_NAME}.PUBLIC.valueSequenceNextTable SET test_id = PUBLIC.test_sequence.nextval;
+UPDATE ${CATALOG_NAME}.PUBLIC.valueSequenceNextTable SET test_id = ${CATALOG_NAME}.PUBLIC.test_sequence.nextval;
