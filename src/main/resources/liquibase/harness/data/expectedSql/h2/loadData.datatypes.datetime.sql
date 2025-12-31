88cb07c496809064b46cf22de045788840224fb0
KushnirykOleh <kushnirykoleh@gmail.com>
2025-12-31 16:35:04 +0200

    revert loadData.datatypes.datetime.sql as it uses dynamic 'now' time

--- src/main/resources/liquibase/harness/data/expectedSql/h2/loadData.datatypes.datetime.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE "PUBLIC"."tbl_pr2387" ("id" INT, "date_col" date, "datetime_col" TIMESTAMP, "time_col" time)
-INSERT INTO "PUBLIC"."tbl_pr2387" ("id", "date_col", "datetime_col", "time_col") VALUES (1, NULL, NULL, NULL)
-INSERT INTO "PUBLIC"."tbl_pr2387" ("id", "date_col", "datetime_col", "time_col") VALUES (2, '2025-12-29', '2025-12-29 17:49:52.623', '17:49:52')
-INSERT INTO "PUBLIC"."tbl_pr2387" ("id", "date_col", "datetime_col", "time_col") VALUES (3, '2022-09-13', '2022-09-13 12:34:56.789123', '12:34:56')
