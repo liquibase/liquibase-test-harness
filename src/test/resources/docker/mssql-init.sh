036c789e909a32023d598f514d31f469e9238a36
Taras Amelianovych <97164517+Tamelianovych@users.noreply.github.com>
2026-07-13 11:58:10 +0200

    INT-2129 adding mssql 2025 (#1359)
    * INT-2129 adding mssql 2025

--- src/test/resources/docker/mssql-init.sh
+++ src/test/resources/docker/mssql-init.sh
@@ -2,11 +2,20 @@
 
 /opt/mssql/bin/sqlservr &
 
+# Pick the sqlcmd client available in this image. SQL Server 2017/2019/2022 ship
+# /opt/mssql-tools; SQL Server 2025 ships only /opt/mssql-tools18, which defaults to
+# encrypted connections and needs -C to trust the self-signed server certificate.
+if [ -x /opt/mssql-tools/bin/sqlcmd ]; then
+    SQLCMD="/opt/mssql-tools/bin/sqlcmd"
+else
+    SQLCMD="/opt/mssql-tools18/bin/sqlcmd -C"
+fi
+
 echo "Waiting for server to start...."
 #do this in a loop because the timing for when the SQL instance is ready is indeterminate
 for i in {1..50};
 do
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -d master -i /docker-entrypoint-initdb.d/mssql-init.sql
+    ${SQLCMD} -S localhost -U sa -P ${SA_PASSWORD} -d master -i /docker-entrypoint-initdb.d/mssql-init.sql
     if [ $? -eq 0 ]
     then
         echo "mssql-init.sh completed"
